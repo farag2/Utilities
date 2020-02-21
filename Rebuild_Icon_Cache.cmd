883caa4e85d9a843d998c03a5cc7510457d299c5
farag2 <farag2@users.noreply.github.com>
2020-02-21 13:41:00 +0300

    Update Rebuild_Icon_Cache.cmd

--- Rebuild_Icon_Cache.cmd
+++ Rebuild_Icon_Cache.cmd
@@ -1,6 +1,10 @@
-ie4uinit.exe -show
-taskkill /IM explorer.exe /F 
-If exist del /A /F /Q "%localappdata%\IconCache.db"
-If exist del /A /F /Q "%localappdata%\Microsoft\Windows\Explorer\iconcache*"
+@echo off
+taskkill /IM explorer.exe /F
+timeout 2 /nobreak >nul
+DEL /F /Q "%LOCALAPPDATA%\IconCache.db"
+DEL /F /Q "%LOCALAPPDATA%\Microsoft\Windows\Explorer\iconcache_*"
+DEL /F /Q "%LOCALAPPDATA%\Microsoft\Windows\Explorer\thumbcache_*"
+timeout 2 /nobreak >nul
 start explorer.exe
+ie4uinit.exe -show
 pause
