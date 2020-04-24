415b29881620d80a2e4330e8f977ef959913b7e6
farag2 <farag2@users.noreply.github.com>
2020-04-24 20:35:51 +0300

    Update Terminal.cmd

--- Windows Terminal/Terminal.cmd
+++ Windows Terminal/Terminal.cmd
@@ -1,1 +1,1 @@
-robocopy "%~dp0." "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" profiles.json
+robocopy "%~dp0." "%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState" settings.json
