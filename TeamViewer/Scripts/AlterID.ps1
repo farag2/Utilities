1597760ade9ca64639f93bf7f62a5e4c8386b406
farag2 <farag2@users.noreply.github.com>
2020-04-29 22:15:10 +0300

    Add files via upload

--- TeamViewer/Scripts/AlterID.ps1
+++ TeamViewer/Scripts/AlterID.ps1
@@ -1,1 +1,25 @@
+﻿Clear-Host
 
+Start-Process $PSScriptRoot\..\AlterID.exe -ArgumentList "-silent"
+Start-Sleep 5
+do
+{
+	$Process = Get-Process | Where-Object -FilterScript {$_.Name -eq "TeamViewer"}
+	if ($Process)
+	{
+		# "Still running: TeamViewer"
+		Start-Sleep 1
+	}
+}
+until (-not ($Process))
+
+powershell.exe -NoProfile -NoLogo -Command Start-Process powershell -WindowStyle Hidden -ArgumentList "{
+	Start-Sleep -Seconds 3
+	TAKEOWN /F $PSScriptRoot\..\rolloutfile.tv13
+	ICACLS $PSScriptRoot\..\rolloutfile.tv13 --% /grant:r %USERNAME%:F
+	Remove-Item -Path $PSScriptRoot\..\rolloutfile.tv13 -Force
+	Remove-Item -Path HKCU:\Software\TeamViewer -Recurse -Force -ErrorAction Ignore
+	Remove-Item $env:LOCALAPPDATA\TeamViewer -Recurse -Force
+	Remove-Item $env:APPDATA\TeamViewer -Recurse -Force
+	exit
+}" -Verb runAs
---
+++ TeamViewer/Scripts/TeamViewer.ps1
@@ -0,0 +1,23 @@
+﻿Clear-Host
+
+Start-Process $PSScriptRoot\..\TeamViewer.exe
+
+do
+{
+	$Process = Get-Process | Where-Object -FilterScript {$_.Name -eq "TeamViewer"}
+	if ($Process)
+	{
+		# "Still running: TeamViewer"
+		Start-Sleep 1
+	}
+}
+until (-not ($Process))
+
+Start-Sleep -Seconds 3
+TAKEOWN /F $PSScriptRoot\..\rolloutfile.tv13
+ICACLS $PSScriptRoot\..\rolloutfile.tv13 --% /grant:r %USERNAME%:F
+Remove-Item -Path $PSScriptRoot\..\rolloutfile.tv13 -Force
+Remove-Item -Path HKCU:\Software\TeamViewer -Recurse -Force -ErrorAction Ignore
+Remove-Item $env:LOCALAPPDATA\TeamViewer -Recurse -Force
+Remove-Item $env:APPDATA\TeamViewer -Recurse -Force
+exit
