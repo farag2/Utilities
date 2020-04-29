f32e1c8201c5473e05374386a47b7f9f4dedd5f2
farag2 <farag2@users.noreply.github.com>
2020-04-29 22:13:32 +0300

    Delete takeown.cmd

--- TeamViewer/takeown.cmd
+++
@@ -1,7 +0,0 @@
-takeown /F %~dp0rolloutfile.tv13
-icacls %~dp0rolloutfile.tv13 /grant:r %username%:F
-DEL %~dp0rolloutfile.tv13 /F /Q
-
-reg delete HKCU\Software\TeamViewer /F
-RMDIR %LOCALAPPDATA%\TeamViewer /S /Q
-RMDIR %APPDATA%\TeamViewer /S /Q
