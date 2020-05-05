f0bdd2932959dd6bb07d241a7f0a94e5220239d1
farag2 <farag2@users.noreply.github.com>
2020-05-05 23:08:59 +0300

    Add files via upload

---
+++ Wi-Fi.ps1
@@ -0,0 +1,15 @@
+﻿# Get Wi-Fi SSID and Passwords
+if ($PSUICulture -eq "ru-RU")
+{
+	ping.exe | Out-Null
+	$OutputEncoding = [System.Console]::OutputEncoding = [System.Console]::InputEncoding = [System.Text.Encoding]::UTF8
+}
+
+$Profiles = netsh wlan show profiles | Select-String -Pattern "All User Profile" | Foreach-Object -Process {$_.ToString().Split(":")[-1].Trim()}
+$Profiles | Foreach-Object -Process {
+	$Password = netsh wlan show profiles name=$_ key="clear" | Select-String -Pattern "Key Content" | Foreach-Object -Process {$_.ToString().Split(":")[-1].Trim()}
+	[PSCustomObject]@{
+		SSID = $_
+		Password = $Password
+	}
+}
