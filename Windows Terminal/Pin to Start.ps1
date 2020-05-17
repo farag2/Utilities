c46b26318889a7686497328e0f14396f7b9a209a
farag2 <farag2@users.noreply.github.com>
2020-05-17 19:06:01 +0300

    Update Pin to Start.ps1

--- Windows Terminal/Pin to Start.ps1
+++ Windows Terminal/Pin to Start.ps1
@@ -5,46 +5,46 @@
 # Запускайте скрипт после каждого обновления Windows Terminal
 # Inspired by https://lennybacon.com/post/Create-a-link-to-a-UWP-app-to-run-as-administrator/
 
-Clear-Host
-$Error.Clear()
-
-Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Force -ErrorAction Ignore
-Start-Sleep -Seconds 5
-
-$shell = New-Object -ComObject Wscript.Shell
-$shortcut = $shell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk")
-$WindowsTerminalAppID = (Get-StartApps | Where-Object -FilterScript {$_.Name -eq "Windows Terminal"}).AppID[-1]
-$Shortcut.TargetPath = "shell:AppsFolder\$WindowsTerminalAppID"
-$Shortcut.Save()
-
-# Run upcoming the Windows Terminal shortcut as Administrator
-# Запускать будущий ярлык Windows Terminal от имени Администратора
-[byte[]]$bytes = Get-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Encoding Byte -Raw
-$bytes[0x15] = $bytes[0x15] -bor 0x20
-Set-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Value $bytes -Encoding Byte -Force
-
-$DesktopFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name Desktop
-$PackageFullName = (Get-AppxPackage -Name Microsoft.WindowsTerminal).PackageFullName
-$Target = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk"
-
-$shell = New-Object -ComObject Wscript.Shell
-$shortcut = $shell.CreateShortcut("$DesktopFolder\Windows Terminal.lnk")
-$Shortcut.TargetPath = "cmd.exe"
-$shortCut.Arguments = "/c `"$Target`""
-$shortCut.IconLocation = "$env:ProgramFiles\WindowsApps\$PackageFullName\WindowsTerminal.exe"
-# Start cmd window minimized
-# Запускать окно cmd свернутым
-$Shortcut.WindowStyle = 7
-$Shortcut.Save()
-
-# Pin the shortcuts to Start
-# Закрепить ярлыки на начальном экране
-# http://www.technosys.net/products/utils/pintotaskbar
-# SHA256: 6967E7A3C2251812DD6B3FA0265FB7B61AADC568F562A98C50C345908C6E827
 if (Test-Connection -ComputerName google.com -Quiet)
 {
+	Clear-Host
+	$Error.Clear()
+
+	Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Force -ErrorAction Ignore
+	Start-Sleep -Seconds 5
+
+	$shell = New-Object -ComObject Wscript.Shell
+	$shortcut = $shell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk")
+	$WindowsTerminalAppID = (Get-StartApps | Where-Object -FilterScript {$_.Name -eq "Windows Terminal"}).AppID[-1]
+	$Shortcut.TargetPath = "shell:AppsFolder\$WindowsTerminalAppID"
+	$Shortcut.Save()
+
+	# Run upcoming the Windows Terminal shortcut as Administrator
+	# Запускать будущий ярлык Windows Terminal от имени Администратора
+	[byte[]]$bytes = Get-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Encoding Byte -Raw
+	$bytes[0x15] = $bytes[0x15] -bor 0x20
+	Set-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Value $bytes -Encoding Byte -Force
+
+	$DesktopFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name Desktop
+	$PackageFullName = (Get-AppxPackage -Name Microsoft.WindowsTerminal).PackageFullName
+	$Target = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk"
+
+	$shell = New-Object -ComObject Wscript.Shell
+	$shortcut = $shell.CreateShortcut("$DesktopFolder\Windows Terminal.lnk")
+	$Shortcut.TargetPath = "cmd.exe"
+	$shortCut.Arguments = "/c `"$Target`""
+	$shortCut.IconLocation = "$env:ProgramFiles\WindowsApps\$PackageFullName\WindowsTerminal.exe"
+	# Start cmd window minimized
+	# Запускать окно cmd свернутым
+	$Shortcut.WindowStyle = 7
+	$Shortcut.Save()
+
+	# Pin the shortcuts to Start
+	# Закрепить ярлыки на начальном экране
+	# http://www.technosys.net/products/utils/pintotaskbar
 	# Download syspin.exe to the "Downloads" folder
 	# Скачать syspin.exe в папку "Загрузки"
+	# SHA256: 6967E7A3C2251812DD6B3FA0265FB7B61AADC568F562A98C50C345908C6E827
 	$DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
 	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 	$param = @{
@@ -53,16 +53,17 @@ if (Test-Connection -ComputerName google.com -Quiet)
 		Verbose = [switch]::Present
 	}
 	Invoke-WebRequest @param
-}
-Write-Verbose -Message "The `"Windows Terminal`" shortcut is being pinned to Start" -Verbose
-$Arguments = @"
-	"$DesktopFolder\Windows Terminal.lnk" "51201"
+
+	Write-Verbose -Message "The `"Windows Terminal`" shortcut is being pinned to Start" -Verbose
+	$Arguments = @"
+		"$DesktopFolder\Windows Terminal.lnk" "51201"
 "@
-Start-Process -FilePath "$DownloadsFolder\syspin.exe" -WindowStyle Hidden -ArgumentList $Arguments -Wait
+	Start-Process -FilePath "$DownloadsFolder\syspin.exe" -WindowStyle Hidden -ArgumentList $Arguments -Wait
 
-Remove-Item -Path "$DownloadsFolder\syspin.exe" -Force
-Remove-Item -Path "$DesktopFolder\Windows Terminal.lnk" -Force
+	Remove-Item -Path "$DownloadsFolder\syspin.exe" -Force
+	Remove-Item -Path "$DesktopFolder\Windows Terminal.lnk" -Force
 
-# Restart the Start menu
-# Перезапустить меню "Пуск"
-Stop-Process -Name StartMenuExperienceHost -Force
+	# Restart the Start menu
+	# Перезапустить меню "Пуск"
+	Stop-Process -Name StartMenuExperienceHost -Force
+}
