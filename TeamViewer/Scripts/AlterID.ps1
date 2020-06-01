b8d37d02887aff1e2588d1bf24579b76ff312871
Dmitry Nefedov <farag2@users.noreply.github.com>
2020-06-01 13:17:26 +0300

    Add files via upload

--- TeamViewer/Scripts/AlterID.ps1
+++ TeamViewer/Scripts/AlterID.ps1
@@ -1,12 +1,12 @@
-﻿Start-Process $PSScriptRoot\..\AlterID.exe -ArgumentList "-silent" -Wait
+﻿Start-Process -FilePath $PSScriptRoot\..\AlterID.exe -ArgumentList "-silent" -Wait
 
 powershell.exe -NoProfile -NoLogo -Command Start-Process powershell -WindowStyle Hidden -ArgumentList "{
 	Start-Sleep -Seconds 3
-	TAKEOWN /F $PSScriptRoot\..\rolloutfile.tv13
-	ICACLS $PSScriptRoot\..\rolloutfile.tv13 --% /grant:r %USERNAME%:F
-	Remove-Item -Path $PSScriptRoot\..\rolloutfile.tv13 -Force
+	TAKEOWN /F '$PSScriptRoot\..\rolloutfile.tv13'
+	ICACLS '$PSScriptRoot\..\rolloutfile.tv13' --% /grant:r %USERNAME%:F
+	Remove-Item -Path '$PSScriptRoot\..\rolloutfile.tv13' -Force
 	Remove-Item -Path HKCU:\Software\TeamViewer -Recurse -Force -ErrorAction Ignore
 	Remove-Item $env:LOCALAPPDATA\TeamViewer -Recurse -Force
 	Remove-Item $env:APPDATA\TeamViewer -Recurse -Force
 	exit
-}" -Verb runAs
+}" -Verb RunAs
--- TeamViewer/Scripts/TeamViewer.ps1
+++ TeamViewer/Scripts/TeamViewer.ps1
@@ -1,9 +1,9 @@
-﻿Start-Process -FilePath $PSScriptRoot\..\TeamViewer.exe -Wait
+﻿Start-Process -FilePath "$PSScriptRoot\..\TeamViewer.exe" -Wait
 
 Start-Sleep -Seconds 3
-TAKEOWN /F $PSScriptRoot\..\rolloutfile.tv13
-ICACLS $PSScriptRoot\..\rolloutfile.tv13 --% /grant:r %USERNAME%:F
-Remove-Item -Path $PSScriptRoot\..\rolloutfile.tv13 -Force
+TAKEOWN /F "$PSScriptRoot\..\rolloutfile.tv13"
+ICACLS "$PSScriptRoot\..\rolloutfile.tv13" --% /grant:r %USERNAME%:F
+Remove-Item -Path "$PSScriptRoot\..\rolloutfile.tv13" -Force
 Remove-Item -Path HKCU:\Software\TeamViewer -Recurse -Force -ErrorAction Ignore
 Remove-Item $env:LOCALAPPDATA\TeamViewer -Recurse -Force
 Remove-Item $env:APPDATA\TeamViewer -Recurse -Force
