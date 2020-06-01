7f93d440d4518cf99f304c8d3af716d9f79d4a26
Dmitry Nefedov <farag2@users.noreply.github.com>
2020-06-01 13:16:33 +0300

    Add files via upload

--- TeamViewer/_AlterID.cmd
+++ TeamViewer/_AlterID.cmd
@@ -1,4 +1,1 @@
-@ECHO OFF
-CHCP 65001 >nul
-
-powershell.exe -ExecutionPolicy RemoteSigned -NoProfile -NoLogo -WindowStyle Hidden -File ".\Scripts\AlterID.ps1"
+powershell.exe -ExecutionPolicy Bypass -NoProfile -NoLogo -WindowStyle Hidden -File ".\Scripts\AlterID.ps1"
