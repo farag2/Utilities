df87a15600dc3ff0c519f77d412fa58997974201
Dmitry Nefedov <farag2@users.noreply.github.com>
2020-06-04 01:30:26 +0300

    Update Pin to Start UWP App.ps1

--- Pin to Start UWP App.ps1
+++ Pin to Start UWP App.ps1
@@ -1,36 +1,37 @@
-﻿# Make Windows Terminal run as Administrator by default and pin it to Start
-# Make any UWP app run as Administrator by default
-# Run the script after every Windows Terminal update
-# Запускать Windows Terminal от имени администратора по умолчанию и закрепить на начальном экране
-# Запускайте скрипт после каждого обновления Windows Terminal
+# Make any UWP app run as Administrator by default and pin it to Start menu
+# Run the script after every app update
+# Запускать UWP-приложение от имени администратора по умолчанию и закрепить на начальном экране
+# Запускайте скрипт после каждого обновления приложения
 # Inspired by https://lennybacon.com/post/Create-a-link-to-a-UWP-app-to-run-as-administrator/
 
 Clear-Host
 
-Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Force -ErrorAction Ignore
+$appname = 
+
+Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\$appname.lnk" -Force -ErrorAction Ignore
 Stop-Process -Name StartMenuExperienceHost -Force
 
 $shell = New-Object -ComObject Wscript.Shell
-$shortcut = $shell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk")
-$WindowsTerminalAppID = (Get-StartApps | Where-Object -FilterScript {$_.Name -eq "Windows Terminal"}).AppID[-1]
-$Shortcut.TargetPath = "shell:AppsFolder\$WindowsTerminalAppID"
+$shortcut = $shell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\$appname.lnk")
+$appnameAppID = (Get-StartApps | Where-Object -FilterScript {$_.Name -eq "$appname"}).AppID[-1]
+$Shortcut.TargetPath = "shell:AppsFolder\$appnameAppID"
 $Shortcut.Save()
 
-# Run upcoming the Windows Terminal shortcut as Administrator
-# Запускать будущий ярлык Windows Terminal от имени Администратора
-[byte[]]$bytes = Get-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Encoding Byte -Raw
+# Run upcoming the $appname shortcut as Administrator
+# Запускать будущий ярлык $appname от имени Администратора
+[byte[]]$bytes = Get-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\$appname.lnk" -Encoding Byte -Raw
 $bytes[0x15] = $bytes[0x15] -bor 0x20
-Set-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Value $bytes -Encoding Byte -Force
+Set-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\$appname.lnk" -Value $bytes -Encoding Byte -Force
 
 $DesktopFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name Desktop
-$Target = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk"
-$PackageFullName = (Get-AppxPackage -Name Microsoft.WindowsTerminal).PackageFullName
+$Target = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\$appname.lnk"
+$PackageFullName = (Get-AppxPackage -Name $appname).PackageFullName
 
 $Shell = New-Object -ComObject Wscript.Shell
-$Shortcut = $Shell.CreateShortcut("$DesktopFolder\Windows Terminal.lnk")
+$Shortcut = $Shell.CreateShortcut("$DesktopFolder\$appname.lnk")
 $Shortcut.TargetPath = "cmd.exe"
 $ShortCut.Arguments = "/c `"$Target`""
-$ShortCut.IconLocation = "$env:ProgramFiles\WindowsApps\$PackageFullName\WindowsTerminal.exe"
+$ShortCut.IconLocation = "$env:ProgramFiles\WindowsApps\$PackageFullName\$appname.exe"
 # Start cmd window minimized
 # Запускать окно cmd свернутым
 $Shortcut.WindowStyle = 7
@@ -59,14 +60,14 @@ else
 	Remove-Item -Path "$DesktopFolder\Windows Terminal.lnk" -Force
 	break
 }
-Write-Verbose -Message "The `"Windows Terminal`" shortcut is being pinned to Start" -Verbose
+Write-Verbose -Message "The `"$appname`" shortcut is being pinned to Start" -Verbose
 $Arguments = @"
-	"$DesktopFolder\Windows Terminal.lnk" "51201"
+	"$DesktopFolder\$appname.lnk" "51201"
 "@
 Start-Process -FilePath "$DownloadsFolder\syspin.exe" -WindowStyle Hidden -ArgumentList $Arguments -Wait
 
 Remove-Item -Path "$DownloadsFolder\syspin.exe" -Force
-Remove-Item -Path "$DesktopFolder\Windows Terminal.lnk" -Force
+Remove-Item -Path "$DesktopFolder\$appname.lnk" -Force
 
 # Restart the Start menu
 # Перезапустить меню "Пуск"
