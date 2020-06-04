932195750b491c431f2ca62cb64f041caa396cb3
Dmitry Nefedov <farag2@users.noreply.github.com>
2020-06-04 19:23:50 +0300

    Add files via upload

---
+++ VS Code/Install Extensions.ps1
@@ -0,0 +1,12 @@
+﻿# Install VS Code extensions
+$Extensions = @(
+	# Локализация
+	"ms-ceintl.vscode-language-pack-ru",
+	# Markdown
+	"DavidAnson.vscode-markdownlint",
+	"ms-vscode.PowerShell"
+)
+foreach ($Extension in $Extensions)
+{
+	& "${env:ProgramFiles}\Microsoft VS Code\bin\code.cmd" --install-extension $Extension
+}
