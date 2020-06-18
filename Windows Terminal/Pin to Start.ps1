c410dff8f6d770d01afff3033221520b9c7f10a7
Dmitry Nefedov <farag2@users.noreply.github.com>
2020-06-18 10:35:55 +0300

    Update Pin to Start.ps1

--- Windows Terminal/Pin to Start.ps1
+++ Windows Terminal/Pin to Start.ps1
@@ -1,4 +1,4 @@
-﻿# Make Windows Terminal run as Administrator by default and pin it to Start
+# Make Windows Terminal run as Administrator by default and pin it to Start
 # Run the script after every Windows Terminal update
 # Запускать Windows Terminal от имени администратора по умолчанию и закрепить на начальном экране
 # Запускайте скрипт после каждого обновления Windows Terminal
@@ -15,21 +15,45 @@ Stop-Process -Name StartMenuExperienceHost -Force
 # Скачать syspin.exe в папку "Загрузки"
 # http://www.technosys.net/products/utils/pintotaskbar
 # SHA256: 6967E7A3C2251812DD6B3FA0265FB7B61AADC568F562A98C50C345908C6E827
-if (Test-Connection -ComputerName google.com -Quiet)
+if (Test-Path -Path $PSScriptRoot\syspin.exe)
 {
-	$DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
-	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-	$Parameters = @{
-		Uri = "https://github.com/farag2/Windows-10-Setup-Script/raw/master/Start%20menu%20pinning/syspin.exe"
-		OutFile = "$DownloadsFolder\syspin.exe"
-		Verbose = [switch]::Present
-	}
-	Invoke-WebRequest @Parameters
+	$syspin = $true
 }
 else
 {
-	Write-Warning -Message "No internet connection"
-	break
+	try
+	{
+		# Downloading syspin.exe
+		# Скачиваем syspin.exe
+		# http://www.technosys.net/products/utils/pintotaskbar
+		# SHA256: 6967E7A3C2251812DD6B3FA0265FB7B61AADC568F562A98C50C345908C6E827
+		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+		if ((Invoke-WebRequest -Uri https://www.google.com -UseBasicParsing -DisableKeepAlive -Method Head).StatusDescription)
+		{
+			$Parameters = @{
+				Uri = "https://github.com/farag2/Windows-10-Setup-Script/raw/master/Start%20menu%20pinning/syspin.exe"
+				OutFile = "$PSScriptRoot\syspin.exe"
+				Verbose = [switch]::Present
+			}
+			Invoke-WebRequest @Parameters
+			$syspin = $true
+		}
+	}
+	catch
+	{
+		if ($Error.Exception.Status -eq "NameResolutionFailure")
+		{
+			if ($RU)
+			{
+				Write-Warning -Message "Отсутствует интернет-соединение" -ErrorAction SilentlyContinue
+			}
+			else
+			{
+				Write-Warning -Message "No Internet connection" -ErrorAction SilentlyContinue
+			}
+			break
+		}
+	}
 }
 
 Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows Terminal*.lnk" -Force
