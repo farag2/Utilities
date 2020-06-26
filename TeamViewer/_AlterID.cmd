3489c0d2e62b641f9b24bbacb98a86b430b77f80
Dmitry Nefedov <10544660+farag2@users.noreply.github.com>
2020-06-26 15:34:32 +0300

    Add files via upload

--- TeamViewer/_AlterID.cmd
+++ TeamViewer/_AlterID.cmd
@@ -1,1 +1,2 @@
+CD /D "%~dp0"
 powershell.exe -ExecutionPolicy Bypass -NoProfile -NoLogo -WindowStyle Hidden -File ".\Scripts\AlterID.ps1"
--- TeamViewer/_Start.cmd
+++ TeamViewer/_Start.cmd
@@ -1,6 +1,2 @@
 CD /D "%~dp0"
-
-REG QUERY "HKU\S-1-5-19\Environment" >nul
-IF %ERRORLEVEL% NEQ 0 powershell.exe -WindowStyle Hidden -NoProfile -NoLogo -Command "Start-Process -Verb RunAS -WindowStyle Hidden -FilePath '%0'" & EXIT
-
 powershell.exe -ExecutionPolicy Bypass -NoProfile -NoLogo -WindowStyle Hidden -File ".\Scripts\TeamViewer.ps1"
