5aa30e0d0dc45b16b023bb8f74dc820220fc0263
Dmitry Nefedov <10544660+farag2@users.noreply.github.com>
2020-07-02 10:07:14 +0300

    Add files via upload

---
+++ Download Firefox.ps1
@@ -0,0 +1,18 @@
+﻿# Downloading Firefox.msi
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
+$Arch = "win64"
+$Lang = "ru"
+$Parameters = @{
+	Uri = "https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=$Arch&lang=$Lang"
+	OutFile = "$DownloadsFolder\Firefox.msi"
+	Verbose = [switch]::Present
+}
+Invoke-WebRequest @Parameters
+
+# Extracting Acrobat_DC_Web_WWMUI.exe to the "Downloads folder\AcrobatTemp" folder
+$ExtractPath = "$DownloadsFolder\Firefox"
+Start-Process -FilePath "$DownloadsFolder\Firefox.msi" -ArgumentList "EXTRACT_DIR=$ExtractPath" -Wait
+
+# Removing unnecessary files and folders
+Remove-Item -Path "$DownloadsFolder\Firefox.msi", "$DownloadsFolder\Firefox\postSigningData" -Force
