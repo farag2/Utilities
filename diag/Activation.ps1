48ca054b280d3b5a2ac2e788b3fe5db69aa02aba
Dmitry Nefedov <10544660+farag2@users.noreply.github.com>
2020-08-04 10:35:03 +0300

    Add files via upload

---
+++ diag/Activation.ps1
@@ -0,0 +1,4 @@
+﻿New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Adobe\Adobe Acrobat\DC\Activation" -Name IsAMTEnforced -PropertyType DWord -Value 1 -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Adobe\Adobe Acrobat\DC\Activation" -Name DisabledActivation -PropertyType DWord -Value 1 -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Adobe\Adobe Acrobat\DC\Activation" -Name IsAMTEnforced -PropertyType DWord -Value 1 -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Adobe\Adobe Acrobat\DC\Activation" -Name DisabledActivation -PropertyType DWord -Value 1 -Force
