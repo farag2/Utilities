7c1e669a5519ac14d37f8669b5b8cf07c4a4c86d
Dmitry Nefedov <10544660+farag2@users.noreply.github.com>
2020-09-24 09:24:17 +0300

    Add files via upload

--- Windows Terminal/Pin to Start.ps1
+++ Windows Terminal/Pin to Start.ps1
@@ -1,7 +1,9 @@
-# Make Windows Terminal run as Administrator by default and pin it to Start
-# Run the script after every Windows Terminal update
-# Запускать Windows Terminal от имени администратора по умолчанию и закрепить на начальном экране
-# Запускайте скрипт после каждого обновления Windows Terminal
+﻿<#
+	Make Windows Terminal run as Administrator by default and pin it to Start
+	Run the script after every Windows Terminal update
+	Запускать Windows Terminal от имени администратора по умолчанию и закрепить на начальном экране
+	Запускайте скрипт после каждого обновления Windows Terminal
+#>
 
 Clear-Host
 
@@ -9,82 +11,72 @@ Clear-Host
 # Перезапустить меню "Пуск"
 Stop-Process -Name StartMenuExperienceHost -Force
 
-# Pin the second shortcut to Start
-# Закрепить второй ярлык на начальном экране
-# Download syspin.exe to the "Downloads" folder
-# Скачать syspin.exe в папку "Загрузки"
-# http://www.technosys.net/products/utils/pintotaskbar
-# SHA256: 6967E7A3C2251812DD6B3FA0265FB7B61AADC568F562A98C50C345908C6E827
-if (Test-Path -Path $PSScriptRoot\syspin.exe)
+<#
+	Pin the "Windows Terminal" shortcut to Start within syspin
+	Закрепить ярлык "Windows Terminal" на начальном экране с помощью syspin
+
+	http://www.technosys.net/products/utils/pintotaskbar
+	SHA256: 6967E7A3C2251812DD6B3FA0265FB7B61AADC568F562A98C50C345908C6E827
+#>
+try
 {
-	$syspin = $true
-}
-else
-{
-	try
+	# Downloading syspin
+	# Скачиваем syspin
+	if ((Invoke-WebRequest -Uri https://www.google.com -UseBasicParsing -DisableKeepAlive -Method Head).StatusDescription)
 	{
-		# Downloading syspin.exe
-		# Скачиваем syspin.exe
-		# http://www.technosys.net/products/utils/pintotaskbar
-		# SHA256: 6967E7A3C2251812DD6B3FA0265FB7B61AADC568F562A98C50C345908C6E827
+		$DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
+
 		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-		if ((Invoke-WebRequest -Uri https://www.google.com -UseBasicParsing -DisableKeepAlive -Method Head).StatusDescription)
-		{
-			$Parameters = @{
-				Uri = "https://github.com/farag2/Windows-10-Setup-Script/raw/master/Start%20menu%20pinning/syspin.exe"
-				OutFile = "$PSScriptRoot\syspin.exe"
-				Verbose = [switch]::Present
-			}
-			Invoke-WebRequest @Parameters
-			$syspin = $true
+		$Parameters = @{
+			Uri = "https://github.com/farag2/Windows-10-Setup-Script/raw/master/Start%20menu%20pinning/syspin.exe"
+			OutFile = "$DownloadsFolder\syspin.exe"
+			Verbose = [switch]::Present
 		}
-	}
-	catch
-	{
-		if ($Error.Exception.Status -eq "NameResolutionFailure")
-		{
-			if ($RU)
-			{
-				Write-Warning -Message "Отсутствует интернет-соединение" -ErrorAction SilentlyContinue
-			}
-			else
-			{
-				Write-Warning -Message "No Internet connection" -ErrorAction SilentlyContinue
-			}
-			break
-		}
+		Invoke-WebRequest @Parameters
 	}
 }
-
-if ($syspin -eq $true)
+catch
 {
-	Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows Terminal*.lnk" -Force
-
-	$PackageFullName = (Get-AppxPackage -Name Microsoft.WindowsTerminal).PackageFullName
-
-	$Shell = New-Object -ComObject Wscript.Shell
-	$Shortcut = $Shell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk")
-	$Shortcut.TargetPath = "powershell.exe"
-	$ShortCut.Arguments = "-WindowStyle Hidden -Command wt"
-	$ShortCut.IconLocation = "$env:ProgramFiles\WindowsApps\$PackageFullName\WindowsTerminal.exe"
-	$Shortcut.Save()
-
-	# Run the Windows Terminal shortcut as Administrator
-	# Запускать ярлык Windows Terminal от имени Администратора
-	[byte[]]$bytes = Get-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Encoding Byte -Raw
-	$bytes[0x15] = $bytes[0x15] -bor 0x20
-	Set-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Value $bytes -Encoding Byte -Force
-
-
-	Write-Verbose -Message "The `"Windows Terminal`" shortcut is being pinned to Start" -Verbose
-	$Arguments = @"
-		"$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" "51201"
-"@
-	Start-Process -FilePath "$PSScriptRoot\syspin.exe" -WindowStyle Hidden -ArgumentList $Arguments -Wait
-
-	# Remove-Item -Path "$PSScriptRoot\syspin.exe" -Force
-
-	# Restart the Start menu
-	# Перезапустить меню "Пуск"
-	Stop-Process -Name StartMenuExperienceHost -Force
+	if ($Error.Exception.Status -eq "NameResolutionFailure")
+	{
+		if ($RU)
+		{
+			Write-Warning -Message "Отсутствует интернет-соединение" -ErrorAction SilentlyContinue
+		}
+		else
+		{
+			Write-Warning -Message "No Internet connection" -ErrorAction SilentlyContinue
+		}
+		break
+	}
 }
+
+Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows Terminal*.lnk" -Force
+
+$PackageFullName = (Get-AppxPackage -Name Microsoft.WindowsTerminal).PackageFullName
+
+$Shell = New-Object -ComObject Wscript.Shell
+$Shortcut = $Shell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk")
+$Shortcut.TargetPath = "powershell.exe"
+$ShortCut.Arguments = "-WindowStyle Hidden -Command wt"
+$ShortCut.IconLocation = "$env:ProgramFiles\WindowsApps\$PackageFullName\WindowsTerminal.exe"
+$Shortcut.Save()
+
+# Run the Windows Terminal shortcut as Administrator
+# Запускать ярлык Windows Terminal от имени Администратора
+[byte[]]$bytes = Get-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Encoding Byte -Raw
+$bytes[0x15] = $bytes[0x15] -bor 0x20
+Set-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Value $bytes -Encoding Byte -Force
+
+
+Write-Verbose -Message "The `"Windows Terminal`" shortcut is being pinned to Start" -Verbose
+$Arguments = @"
+	"$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" "51201"
+"@
+Start-Process -FilePath "$DownloadsFolder\syspin.exe" -WindowStyle Hidden -ArgumentList $Arguments -Wait
+
+Remove-Item -Path "$DownloadsFolder\syspin.exe" -Force
+
+# Restart the Start menu
+# Перезапустить меню "Пуск"
+Stop-Process -Name StartMenuExperienceHost -Force
--- Windows Terminal/Settings.ps1
+++ Windows Terminal/Settings.ps1
@@ -1,33 +1,47 @@
 ﻿# https://docs.microsoft.com/en-us/windows/terminal/
 # https://github.com/microsoft/terminal/issues/1555#issuecomment-505157311
 
-# Intalling the latest PSReadLine
-# Установка последней версии PSReadLine
+# Intall PSReadLine 2.0.4
+# https://github.com/PowerShell/PSReadLine/releases
+if (-not (Get-Package -Name NuGet -Force -ErrorAction Ignore))
+{
+	Install-Package -Name NuGet -Force
+}
 Install-Module -Name PSReadLine -RequiredVersion 2.0.4 -Force
 
+# Remove the old PSReadLine 2.0.0
+$PSReadLine = @{
+	ModuleName = "PSReadLine"
+	ModuleVersion = "2.0.0"
+}
+Remove-Module -FullyQualifiedName $PSReadLine
+Get-InstalledModule -Name PSReadline -AllVersions | Where-Object -FilterScript {$_.Version -eq "2.0.0"} | Uninstall-Module
+Remove-Item -Path $env:ProgramFiles\WindowsPowerShell\Modules\PSReadline\2.0.0 -Recurse -Force -ErrorAction Ignore
+
+# Download Windows95.gif
+# https://github.com/farag2/Utilities/tree/master/Windows%20Terminal
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$param = @{
+$Parameters = @{
 	Uri = "https://github.com/farag2/Utilities/raw/master/Windows%20Terminal/Windows95.gif"
 	OutFile = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\RoamingState\Windows95.gif"
 	Verbose = [switch]::Present
 }
-Invoke-WebRequest @param
+Invoke-WebRequest @Parameters
 
 $JsonPath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
 # Remove all comments to parse JSON file
-# Удалить все комментарии, чтобы пропарсить JSON-файл
 if (Get-Content -Path $JsonPath | Select-String -Pattern "//" -SimpleMatch)
 {
-	Set-Content -Path $JsonPath -Value (Get-Content -Path $JsonPath | Select-String -Pattern "//" -NotMatch)
+	Set-Content -Path $JsonPath -Value (Get-Content -Path $JsonPath | Select-String -Pattern "//" -NotMatch) -Force
 }
 
 # Delete all blank lines from JSON file
-# Удалить все пустые строки в JSON-файле
-(Get-Content -Path $JsonPath) | Where-Object -FilterScript {$_.Trim(" `t")} | Set-Content -Path $JsonPath
+(Get-Content -Path $JsonPath) | Where-Object -FilterScript {$_.Trim(" `t")} | Set-Content -Path $JsonPath -Force
 
 $Terminal = Get-Content -Path $JsonPath | ConvertFrom-Json
 
+# Close tab
 if (-not ($Terminal.keybindings | Where-Object -FilterScript {$_.command -eq "closeTab"} | Where-Object -FilterScript {$_.keys -eq "ctrl+w"}))
 {
 	$closeTab = [PSCustomObject]@{
@@ -37,6 +51,7 @@ if (-not ($Terminal.keybindings | Where-Object -FilterScript {$_.command -eq "cl
 	$Terminal.keybindings += $closeTab
 }
 
+# New tab
 if (-not ($Terminal.keybindings | Where-Object -FilterScript {$_.command -eq "newTab"} | Where-Object -FilterScript {$_.keys -eq "ctrl+t"}))
 {
 	$newTab = [PSCustomObject]@{
@@ -46,6 +61,7 @@ if (-not ($Terminal.keybindings | Where-Object -FilterScript {$_.command -eq "ne
 	$Terminal.keybindings += $newTab
 }
 
+# Find
 if (-not ($Terminal.keybindings | Where-Object -FilterScript {$_.command -eq "find"} | Where-Object -FilterScript {$_.keys -eq "ctrl+f"}))
 {
 	$find = [PSCustomObject]@{
@@ -55,6 +71,7 @@ if (-not ($Terminal.keybindings | Where-Object -FilterScript {$_.command -eq "fi
 	$Terminal.keybindings += $find
 }
 
+# Split pane
 if (-not ($Terminal.keybindings | Where-Object -FilterScript {$_.command.action -eq "splitPane"} | Where-Object -FilterScript {$_.command.split -eq "auto"} | Where-Object -FilterScript {$_.command.splitMode -eq "duplicate"}))
 {
 	$split = [PSCustomObject]@{
@@ -69,6 +86,7 @@ if (-not ($Terminal.keybindings | Where-Object -FilterScript {$_.command.action 
 	$Terminal.keybindings += $splitPane
 }
 
+# No confirmation when closing all tabs
 if ($Terminal.confirmCloseAllTabs)
 {
 	$Terminal.confirmCloseAllTabs = $false
@@ -78,6 +96,7 @@ else
 	$Terminal | Add-Member -MemberType NoteProperty -Name confirmCloseAllTabs -Value $false -Force
 }
 
+# Show tabs in title bar
 if ($Terminal.showTabsInTitlebar)
 {
 	$Terminal.showTabsInTitlebar = $false
@@ -87,7 +106,8 @@ else
 	$Terminal | Add-Member -MemberType NoteProperty -Name showTabsInTitlebar -Value $false -Force
 }
 
-# PowerShell
+# The PowerShell profile settings
+# Set Windows95.gif as a background image
 if ($Terminal.profiles.list[0].backgroundImage)
 {
 	$Terminal.profiles.list[0].backgroundImage = "ms-appdata:///roaming/Windows95.gif"
@@ -97,6 +117,7 @@ else
 	$Terminal.profiles.list[0] | Add-Member -MemberType NoteProperty -Name backgroundImage -Value "ms-appdata:///roaming/Windows95.gif" -Force
 }
 
+# Background image alignment
 if ($Terminal.profiles.list[0].backgroundImageAlignment)
 {
 	$Terminal.profiles.list[0].backgroundImageAlignment = "bottomRight"
@@ -106,6 +127,7 @@ else
 	$Terminal.profiles.list[0] | Add-Member -MemberType NoteProperty -Name backgroundImageAlignment -Value bottomRight -Force
 }
 
+# Background image opacity
 $Value = 0.3
 if ($Terminal.profiles.list[0].backgroundImageOpacity)
 {
@@ -116,6 +138,7 @@ else
 	$Terminal.profiles.list[0] | Add-Member -MemberType NoteProperty -Name backgroundImageOpacity -Value 0.3 -Force
 }
 
+# Background image stretch mode
 if ($Terminal.profiles.list[0].backgroundImageStretchMode)
 {
 	$Terminal.profiles.list[0].backgroundImageStretchMode = "none"
@@ -125,6 +148,7 @@ else
 	$Terminal.profiles.list[0] | Add-Member -MemberType NoteProperty -Name backgroundImageStretchMode -Value none -Force
 }
 
+# Starting directory
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name Desktop
 if ($Terminal.profiles.list[0].startingDirectory)
 {
@@ -135,6 +159,7 @@ else
 	$Terminal.profiles.list[0] | Add-Member -MemberType NoteProperty -Name startingDirectory -Value $DownloadsFolder -Force
 }
 
+# Use acrylic
 if ($Terminal.profiles.list[0].useAcrylic)
 {
 	$Terminal.profiles.list[0].useAcrylic = $true
@@ -144,6 +169,7 @@ else
 	$Terminal.profiles.list[0] | Add-Member -MemberType NoteProperty -Name useAcrylic -Value $true -Force
 }
 
+# Acrylic opacity
 $Value = 0.75
 if ($Terminal.profiles.list[0].acrylicOpacity)
 {
@@ -154,7 +180,8 @@ else
 	$Terminal.profiles.list[0] | Add-Member -MemberType NoteProperty -Name acrylicOpacity -Value 0.75 -Force
 }
 
-# cmd
+# The CMD profile settings
+# Set Windows95.gif as a background image
 if ($Terminal.profiles.list[1].backgroundImage)
 {
 	$Terminal.profiles.list[1].backgroundImage = "ms-appdata:///roaming/Windows95.gif"
@@ -164,6 +191,7 @@ else
 	$Terminal.profiles.list[1] | Add-Member -MemberType NoteProperty -Name backgroundImage -Value "ms-appdata:///roaming/Windows95.gif" -Force
 }
 
+# Background image alignment
 if ($Terminal.profiles.list[1].backgroundImageAlignment)
 {
 	$Terminal.profiles.list[1].backgroundImageAlignment = "bottomRight"
@@ -173,6 +201,7 @@ else
 	$Terminal.profiles.list[1] | Add-Member -MemberType NoteProperty -Name backgroundImageAlignment -Value bottomRight -Force
 }
 
+# Background image opacity
 $Value = 0.3
 if ($Terminal.profiles.list[1].backgroundImageOpacity)
 {
@@ -183,6 +212,7 @@ else
 	$Terminal.profiles.list[1] | Add-Member -MemberType NoteProperty -Name backgroundImageOpacity -Value 0.3 -Force
 }
 
+# Background image stretch mode
 if ($Terminal.profiles.list[1].backgroundImageStretchMode)
 {
 	$Terminal.profiles.list[1].backgroundImageStretchMode = "none"
@@ -192,6 +222,7 @@ else
 	$Terminal.profiles.list[1] | Add-Member -MemberType NoteProperty -Name backgroundImageStretchMode -Value none -Force
 }
 
+# Starting directory
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name Desktop
 if ($Terminal.profiles.list[1].startingDirectory)
 {
@@ -202,6 +233,7 @@ else
 	$Terminal.profiles.list[1] | Add-Member -MemberType NoteProperty -Name startingDirectory -Value $DownloadsFolder -Force
 }
 
+# Use acrylic
 if ($Terminal.profiles.list[1].useAcrylic)
 {
 	$Terminal.profiles.list[1].useAcrylic = $true
@@ -211,6 +243,7 @@ else
 	$Terminal.profiles.list[1] | Add-Member -MemberType NoteProperty -Name useAcrylic -Value $true -Force
 }
 
+# Acrylic opacity
 $Value = 0.75
 if ($Terminal.profiles.list[1].acrylicOpacity)
 {
@@ -221,6 +254,7 @@ else
 	$Terminal.profiles.list[1] | Add-Member -MemberType NoteProperty -Name acrylicOpacity -Value 0.75 -Force
 }
 
+# Hide Azure Cloud Shell profile
 if ($Terminal.profiles.list[2].hidden)
 {
 	$Terminal.profiles.list[2].hidden = $true
