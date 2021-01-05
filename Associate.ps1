51fdb4cf6d38b695fbcd3177bbd01a92f5254c1a
Dmitry Nefedov <nefedovds@ya.ru>
2021-01-05 22:15:08 +0300

    Create Associate.ps1

---
+++ Associate.ps1
@@ -0,0 +1,52 @@
+# Associate the following extensions with Notepad++ (as an example)
+
+# Get the associations table
+# https://www.pstips.net/assoc-ftype.html
+$ftypeMap = @{}
+& cmd.exe /c ftype | ForEach-Object -Process {
+	$FileType, $Executable = $_.split("=")
+	$ftypeMap.Add($FileType,$Executable)
+}
+
+& cmd.exe /c assoc | ForEach-Object -Process {
+	$Extension, $FileType = $_.split("=")
+	[PSCustomObject]@{
+		Extension  = $Extension
+		FileType   = $FileType
+		Executable = $ftypeMap[$FileType]
+	}
+}
+
+$Extensions = @(
+	".cfg",
+	".ini",
+	".json",
+	".log",
+	".nfo",
+	".txt",
+	".xml"
+)
+foreach ($Extension in $Extensions)
+{
+	$FileType = $Extension.Split(".")[1] + "file"
+	& cmd.exe /c assoc $Extension=$FileType
+	& cmd.exe /c assoc $FileType\DefaultIcon=%ProgramFiles%\Notepad++\notepad++.exe,0
+	& cmd.exe /c ftype $FileType="%ProgramFiles%\Notepad++\notepad++.exe" "%1"
+}
+
+# The following extensions cannot be associated within the same method like the previous ones
+$Extensions = @{
+	".ps1"  = "Microsoft.PowerShellScript.1"
+	".psd1" = "Microsoft.PowerShellData.1"
+	".psm1" = "Microsoft.PowerShellModule.1"
+}
+foreach ($Extension in $Extensions.Keys)
+{
+	$FileType = $Extensions[$Extension]
+	& cmd.exe /c assoc $Extensions.Keys=$FileType
+	& cmd.exe /c "assoc $FileType\DefaultIcon=%ProgramFiles%\Notepad++\notepad++.exe,0"
+	& cmd.exe /c "ftype $FileType=`"%ProgramFiles%\Notepad++\notepad++.exe`" `"%1`""
+}
+
+# Rebuild icon cache. For Windows 10 only
+& cmd.exe /c ie4uinit.exe -show
