5e1dda42d37ab088bfbabd021a1c0779bfeaa75d
Dmitry Nefedov <nefedovds@ya.ru>
2021-01-27 22:27:20 +0300

    Create TaskbarX.ps1

---
+++ Download/TaskbarX.ps1
@@ -0,0 +1,26 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$Tag = ((Invoke-RestMethod -Uri "https://api.github.com/repos/ChrisAnd1998/TaskbarX/releases") | Where-Object -FilterScript {$_.prerelease -eq $false}).tag_name[0]
+$URL = (((Invoke-WebRequest -Uri "https://api.github.com/repos/ChrisAnd1998/TaskbarX/releases" | ConvertFrom-Json) | Where-Object -FilterScript {$_.prerelease -eq $false} | Where-Object -FilterScript {$_.tag_name -eq $Tag}).assets | Where-Object -FilterScript {$_.browser_download_url -like "*x64.zip"}).browser_download_url
+
+$DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
+$Parameters = @{
+	Uri = $URL
+	OutFile = "$DownloadsFolder\TaskbarX_v$Tag.zip"
+	Verbose = [switch]::Present
+}
+Invoke-WebRequest @Parameters
+
+$Parameters = @{
+	Path = "$DownloadsFolder\TaskbarX_v$Tag.zip"
+	DestinationPath = "$DownloadsFolder\TaskbarX"
+	Force = [switch]::Present
+	Verbose = [switch]::Present
+}
+Expand-Archive @Parameters
+
+Remove-Item -Path "$DownloadsFolder\TaskbarX_v$Tag.zip" -Force
+
+Move-Item -Path "$DownloadsFolder\TaskbarX" -Destination $env:ProgramFiles -Force
+
+Start-Process -FilePath "$env:ProgramFiles\TaskbarX\TaskbarX Configurator.exe"
