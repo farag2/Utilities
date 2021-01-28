b3a296de5b9f6f10ea92fa3955a46ca114954a6f
Dmitry Nefedov <nefedovds@ya.ru>
2021-01-28 10:59:10 +0300

    Update TaskbarX.ps1

--- Download/TaskbarX.ps1
+++ Download/TaskbarX.ps1
@@ -1,7 +1,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$Tag = ((Invoke-RestMethod -Uri "https://api.github.com/repos/ChrisAnd1998/TaskbarX/releases") | Where-Object -FilterScript {$_.prerelease -eq $false}).tag_name[0]
-$URL = (((Invoke-WebRequest -Uri "https://api.github.com/repos/ChrisAnd1998/TaskbarX/releases" | ConvertFrom-Json) | Where-Object -FilterScript {$_.prerelease -eq $false} | Where-Object -FilterScript {$_.tag_name -eq $Tag}).assets | Where-Object -FilterScript {$_.browser_download_url -like "*x64.zip"}).browser_download_url
+$Tag = ((Invoke-RestMethod -Uri "https://api.github.com/repos/ChrisAnd1998/TaskbarX/releases" -UseBasicParsing) | Where-Object -FilterScript {$_.prerelease -eq $false}).tag_name[0]
+$URL = (((Invoke-WebRequest -Uri "https://api.github.com/repos/ChrisAnd1998/TaskbarX/releases" -UseBasicParsing | ConvertFrom-Json) | Where-Object -FilterScript {$_.prerelease -eq $false} | Where-Object -FilterScript {$_.tag_name -eq $Tag}).assets | Where-Object -FilterScript {$_.browser_download_url -like "*x64.zip"}).browser_download_url
 
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
 $Parameters = @{
@@ -17,7 +17,10 @@ $Parameters = @{
 	Force = [switch]::Present
 	Verbose = [switch]::Present
 }
-Expand-Archive @Parameters
+if (Test-Path "$DownloadsFolder\TaskbarX_v$Tag.zip")
+{
+    Expand-Archive @Parameters
+}
 
 Remove-Item -Path "$DownloadsFolder\TaskbarX_v$Tag.zip" -Force
 
