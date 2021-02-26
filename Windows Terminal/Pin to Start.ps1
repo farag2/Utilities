5f08d7cb5517bb37befb563b631311bbb1baafd0
Dmitry Nefedov <nefedovds@ya.ru>
2021-02-26 10:37:14 +0300

    Update Pin to Start.ps1

--- Windows Terminal/Pin to Start.ps1
+++ Windows Terminal/Pin to Start.ps1
@@ -1,60 +1,14 @@
-﻿<#
-	Make Windows Terminal run as Administrator by default and pin it to Start
-	Run the script after every Windows Terminal update
-	Запускать Windows Terminal от имени администратора по умолчанию и закрепить на начальном экране
-	Запускайте скрипт после каждого обновления Windows Terminal
-#>
+# Make Windows Terminal run as Administrator by default and pin it to Start
+# Run the script after every Windows Terminal update
+# Inspired by https://lennybacon.com/posts/create-an-link-to-a-uwp-app-to-run-as-administrator/
 
 Clear-Host
-
-# Restart the Start menu
-# Перезапустить меню "Пуск"
-Stop-Process -Name StartMenuExperienceHost -Force
-
-<#
-	Pin the "Windows Terminal" shortcut to Start within syspin
-	Закрепить ярлык "Windows Terminal" на начальном экране с помощью syspin
-
-	http://www.technosys.net/products/utils/pintotaskbar
-	SHA256: 6967E7A3C2251812DD6B3FA0265FB7B61AADC568F562A98C50C345908C6E827
-#>
-try
-{
-	# Downloading syspin
-	# Скачиваем syspin
-	if ((Invoke-WebRequest -Uri https://www.google.com -UseBasicParsing -DisableKeepAlive -Method Head).StatusDescription)
-	{
-		$DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
-
-		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-		$Parameters = @{
-			Uri = "https://github.com/farag2/Windows-10-Sophia-Script/raw/master/syspin/syspin.exe"
-			OutFile = "$DownloadsFolder\syspin.exe"
-			Verbose = [switch]::Present
-		}
-		Invoke-WebRequest @Parameters
-	}
-}
-catch
-{
-	if ($Error.Exception.Status -eq "NameResolutionFailure")
-	{
-		if ($RU)
-		{
-			Write-Warning -Message "Отсутствует интернет-соединение" -ErrorAction SilentlyContinue
-		}
-		else
-		{
-			Write-Warning -Message "No Internet connection" -ErrorAction SilentlyContinue
-		}
-		break
-	}
-}
 
 Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows Terminal*.lnk" -Force
 
 $PackageFullName = (Get-AppxPackage -Name Microsoft.WindowsTerminal).PackageFullName
 
+# Create a Windows Terminal shortcut
 $Shell = New-Object -ComObject Wscript.Shell
 $Shortcut = $Shell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk")
 $Shortcut.TargetPath = "powershell.exe"
@@ -63,20 +17,136 @@ $ShortCut.IconLocation = "$env:ProgramFiles\WindowsApps\$PackageFullName\Windows
 $Shortcut.Save()
 
 # Run the Windows Terminal shortcut as Administrator
-# Запускать ярлык Windows Terminal от имени Администратора
 [byte[]]$bytes = Get-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Encoding Byte -Raw
 $bytes[0x15] = $bytes[0x15] -bor 0x20
 Set-Content -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" -Value $bytes -Encoding Byte -Force
 
+$Parameters = @{
+	Size = "2x2"
+	Column = 0
+	Row = 0
+	AppID = "Microsoft.WindowsTerminal_8wekyb3d8bbwe!App"
+}
 
-Write-Verbose -Message "The `"Windows Terminal`" shortcut is being pinned to Start" -Verbose
-$Arguments = @"
-	"$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Windows PowerShell\Windows Terminal.lnk" "51201"
-"@
-Start-Process -FilePath "$DownloadsFolder\syspin.exe" -WindowStyle Hidden -ArgumentList $Arguments -Wait
+# Valid columns to place tiles in
+$ValidColumns = @(0, 2, 4)
 
-Remove-Item -Path "$DownloadsFolder\syspin.exe" -Force
+[string]$StartLayoutNS = "http://schemas.microsoft.com/Start/2014/StartLayout"
+
+# Add pre-configured hastable to XML
+function Add-Tile
+{
+	param
+	(
+		[string]
+		$Size,
+
+		[int]
+		$Column,
+
+		[int]
+		$Row,
+
+		[string]
+		$AppID
+	)
+
+	[string]$elementName = "start:Tile"
+	[Xml.XmlElement]$Table = $xml.CreateElement($elementName, $StartLayoutNS)
+	$Table.SetAttribute("Size", $Size)
+	$Table.SetAttribute("Column", $Column)
+	$Table.SetAttribute("Row", $Row)
+	$Table.SetAttribute("AppUserModelID", $AppID)
+
+	$Table
+}
+
+# Export the current Start layout
+$StartLayout = "$PSScriptRoot\StartLayout.xml"
+Export-StartLayout -Path $StartLayout -UseDesktopApplicationID
+
+[xml]$XML = Get-Content -Path $StartLayout -Encoding UTF8 -Force
+
+$Groups = $XML.LayoutModificationTemplate.DefaultLayoutOverride.StartLayoutCollection.StartLayout.Group
+
+<#
+if ($Groups)
+{
+	$AppUserModelID = ($Parameters).AppID
+
+	if (-not ($Group.Tile | Where-Object -FilterScript {$_.AppUserModelID -eq $AppUserModelID}))
+	{
+		# Count childnodes to get the minimal tiles in a row
+		$ChildNodesCount = New-Object -TypeName System.Collections.ArrayList($null)
+		foreach ($Group in $Groups)
+		{
+			$ChildNodesCount += $Group.ChildNodes.Count
+		}
+		$Minimum = ($ChildNodesCount | Measure-Object -Minimum).Minimum | Select-Object -First 1
+
+		# A necessary group
+		$GroupMinimum = $XML.LayoutModificationTemplate.DefaultLayoutOverride.StartLayoutCollection.StartLayout.Group | Where-Object -FilterScript {$_.ChildNodes.Count -eq $Minimum} | Select-Object -First 1
+
+		# Calculate current filled columns
+		$CurrentColumns = New-Object -TypeName System.Collections.ArrayList($null)
+		if ($GroupMinimum.Tile)
+		{
+			$CurrentColumns += @($GroupMinimum.Tile.Column)
+		}
+		if ($GroupMinimum.DesktopApplicationTile)
+		{
+			$CurrentColumns += @($GroupMinimum.DesktopApplicationTile.Column)
+		}
+
+		# Calculate current free columns and take the first one
+		$Column = (Compare-Object -ReferenceObject $ValidColumns -DifferenceObject $CurrentColumns).InputObject | Select-Object -First 1
+		# If filled cells contain necessary ones assign the first free cell
+		if ($CurrentColumns -contains $Parameters.Column)
+		{
+			$Parameters.Column = $Column
+		}
+		$GroupMinimum.AppendChild((Add-Tile @Parameters)) | Out-Null
+	}
+}
+else
+{
+	# Create a new group
+	[Xml.XmlElement]$Groups = $XML.CreateElement("start:Group", $StartLayoutNS)
+	$Groups.SetAttribute("Name","")
+	$Groups.AppendChild((Add-Tile @Parameters)) | Out-Null
+	$XML.LayoutModificationTemplate.DefaultLayoutOverride.StartLayoutCollection.StartLayout.AppendChild($Groups) | Out-Null
+}
+#>
+
+# Create a new group
+[Xml.XmlElement]$Groups = $XML.CreateElement("start:Group", $StartLayoutNS)
+$Groups.SetAttribute("Name","")
+$Groups.AppendChild((Add-Tile @Parameters)) | Out-Null
+$XML.LayoutModificationTemplate.DefaultLayoutOverride.StartLayoutCollection.StartLayout.AppendChild($Groups) | Out-Null
+
+$XML.Save($StartLayout)
+
+# Temporarily disable changing the Start menu layout
+if (-not (Test-Path -Path HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer))
+{
+	New-Item -Path HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer -Force
+}
+New-ItemProperty -Path HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer -Name LockedStartLayout -Value 1 -Force
+New-ItemProperty -Path HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer -Name StartLayoutFile -Value $StartLayout -Force
+
+Start-Sleep -Seconds 3
 
 # Restart the Start menu
-# Перезапустить меню "Пуск"
-Stop-Process -Name StartMenuExperienceHost -Force
+Stop-Process -Name StartMenuExperienceHost -Force -ErrorAction Ignore
+
+Start-Sleep -Seconds 3
+
+# Enable changing the Start menu layout
+Remove-ItemProperty -Path HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer -Name LockedStartLayout -Force -ErrorAction Ignore
+Remove-ItemProperty -Path HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer -Name StartLayoutFile -Force -ErrorAction Ignore
+
+Remove-Item -Path $StartLayout -Force
+
+Stop-Process -Name StartMenuExperienceHost -Force -ErrorAction Ignore
+
+Start-Sleep -Seconds 3
