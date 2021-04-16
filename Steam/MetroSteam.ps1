72b88b1075f9c54146aec8ec5c16423678c8940b
Dmitry Nefedov <nefedovds@ya.ru>
2021-04-16 17:05:06 +0300

    Update MetroSteam.ps1

--- Steam/MetroSteam.ps1
+++ Steam/MetroSteam.ps1
@@ -35,121 +35,117 @@ $Parameters = @{
 Expand-Archive @Parameters
 
 <#
-    .SYNOPSIS
-        Копирование или псевдоперемещение файлов и каталогов с сохранением структуры
+	.SYNOPSIS
+		Копирование или псевдоперемещение файлов и каталогов с сохранением структуры
 
-    .Description
-        Скрипт предназначен для копирования или перемещения файлов из одного каталога
-        в другой, при этом сохраняется вложенная структура каталогов. Фактически,
-        перемещение не выполняется, вместо него работает копирование с последующим
-        удалением скопированнных итемов.
+	.Description
+		Скрипт предназначен для копирования или перемещения файлов из одного каталога в другой, при этом сохраняется вложенная структура каталогов
+		Фактически перемещение не выполняется, вместо него работает копирование с последующим удалением скопированнных файлов
 
-    .Parameter Source
-        Необязательный: Исходный каталог для выборки элементов. Указывается либо
-        относительный, либо абсолютный путь.
+	.Parameter Source
+		Необязательный: Исходный каталог для выборки элементов
+		Указывается либо относительный, либо абсолютный путь
 
-    .Parameter Destination
-        Необязательный: Целевой каталог. Указывается либо относительный, либо
-        абсолютный путь.
+	.Parameter Destination
+		Необязательный: Целевой каталог
+		Указывается либо относительный, либо абсолютный путь
 
-    .Parameter Include
-        Необязательный: маска(и) для включения элементов в выборку
+	.Parameter Include
+		Необязательный: маска(и) для включения элементов в выборку
 
-    .Parameter Exclude
-        Необязательный: маска(и) для исключения элементов из выборки
+	.Parameter Exclude
+		Необязательный: маска(и) для исключения элементов из выборки
 
-    .Parameter Delete
-        Переключатель: Если указан, то будут удалены исходные файлы, включенные в выборку,
-        рекурсивно
+	.Parameter Delete
+		Переключатель: если указан, то будут удалены исходные файлы, включенные в выборку, рекурсивно
 
-    .Parameter DeleteEmpty
-        Переключатель: Если указан, то будут удалены рекурсивно пустые подкаталоги в исходном каталоге
+	.Parameter DeleteEmpty
+		Переключатель: если указан, то будут удалены рекурсивно пустые подкаталоги в исходном каталоге
 
-    .Example
-        .\script.ps1 -Source "D:\FOLDER1" -Destination "d:\Folder2" -Include '*.pdf', '*.txt' -Exclude '*_out.*' -Delete
+	.Example
+		.\script.ps1 -Source "D:\FOLDER1" -Destination "d:\Folder2" -Include '*.pdf', '*.txt' -Exclude '*_out.*' -Delete
 
-        Это пример перемещения файлов по маскам '*.pdf','*.txt' с исключением из выборки по маске
-        '*_out.*', с последующим удалением исходных элементов. Пустые каталоги не удаляются
+		Это пример перемещения файлов по маскам '*.pdf','*.txt' с исключением из выборки по маске '*_out.*'
+		с последующим удалением исходных элементов. Пустые каталоги не удаляются
 
-    .Example
-        .\script.ps1
+	.Example
+		.\script.ps1
 
-        Это пример копирования/перемещения файлов/каталогов, с сохранением структуры, но
-        по указанным в самом скрипте параметрам...
+		Это пример копирования/перемещения файлов/каталогов, с сохранением структуры, но по указанным в самом скрипте параметрам
 
-    .Link
-        https://forum.ru-board.com/topic.cgi?forum=62&topic=30859&start=3600#4
+	.Link
+		https://forum.ru-board.com/topic.cgi?forum=62&topic=30859&start=3600#4
 
-    .Notes
-        Created By YuS
+	.Notes
+		Created By YuS
 
-        Version: 1.00
-        Date: 20.01.2021
+		Version: 1.00
+		Date: 20.01.2021
 #>
 function Move-Recursively
 {
-    [CmdletBinding()]
-    param
-    (
-        [string]
-        $Source,
+	[CmdletBinding()]
+	param
+	(
+		[string]
+		$Source,
 
-        [string]
-        $Destination,
+		[string]
+		$Destination,
 
-        [string[]]
-        $Include = '*.*',
+		[string[]]
+		$Include = '*.*',
 
-        [string[]]
-        $Exclude = '',
+		[string[]]
+		$Exclude = '',
 
-        [switch]
-        $Delete,
+		[switch]
+		$Delete,
 
-        [switch]
-        $DeleteEmpty
-    )
-    $src = Get-Item -LiteralPath $Source -Force
+		[switch]
+		$DeleteEmpty
+	)
+	$src = Get-Item -LiteralPath $Source -Force
 
-    # перемещение файлов с сохранением структуры
-    Get-ChildItem -LiteralPath $src.fullname -Include $Include -Exclude $Exclude -Recurse -Force | Copy-Item -Destination {
-        $Folder = Split-Path -Path $_.FullName.Replace("$($src.FullName)",$Destination)
-        if (-not (Test-Path -LiteralPath $Folder))
-        {
-            New-Item -Path $Folder -ItemType Directory -Force
-        }
-        else
-        {
-            $Folder
-        }
-    } -Force #-whatif
+	# перемещение файлов с сохранением структуры
+	Get-ChildItem -LiteralPath $src.fullname -Include $Include -Exclude $Exclude -Recurse -Force | Copy-Item -Destination {
+		$Folder = Split-Path -Path $_.FullName.Replace("$($src.FullName)", $Destination)
 
-    if ($Delete.IsPresent)
-    {
-        # удаляем все скопированные файлы
-        Get-ChildItem -LiteralPath $src.FullName  -Include $Include -Exclude $Exclude -Recurse -Force -File | Remove-Item -Recurse -Force
-    }
-    if ($DeleteEmpty.IsPresent)
-    {
-        # удаляем пустые каталоги
-        Get-ChildItem -LiteralPath $src.FullName -Recurse -Directory -Force | Sort-Object {$_.FullName.Length} -Descending | ForEach-Object -Process {
-            if ($null -eq (Get-ChildItem -LiteralPath $_.FullName -Recurse -force))
-            {
-                Remove-Item -LiteralPath $_.FullName -Force
-            }
-        }
-    }
+		if (-not (Test-Path -LiteralPath $Folder))
+		{
+			New-Item -Path $Folder -ItemType Directory -Force
+		}
+		else
+		{
+			$Folder
+		}
+	} -Force
+
+	if ($Delete.IsPresent)
+	{
+		# удаляем все скопированные файлы
+		Get-ChildItem -LiteralPath $src.FullName -Include $Include -Exclude $Exclude -Recurse -File -Force | Remove-Item -Recurse -Force
+	}
+
+	if ($DeleteEmpty.IsPresent)
+	{
+		# удаляем пустые каталоги
+		Get-ChildItem -LiteralPath $src.FullName -Recurse -Directory -Force | Sort-Object {$_.FullName.Length} -Descending | ForEach-Object -Process {
+			if ($null -eq (Get-ChildItem -LiteralPath $_.FullName -Recurse -Force))
+			{
+				Remove-Item -LiteralPath $_.FullName -Force
+			}
+		}
+	}
 }
 
 $Source = "$DownloadsFolder\Metro\UPMetroSkin-master\Unofficial 4.x Patch\Main Files [Install First]"
 $Destination = "$DownloadsFolder\Metro\metro-for-steam-4.4"
-
-Move-Recursively -Source $Source -Destination $Destination -Delete
+Move-Recursively -Source $Source -Destination $Destination -Delete -DeleteEmpty
 
 # Removing unnecessary files and folders
 Remove-Item -Path "$DownloadsFolder\metro-for-steam.zip", "$DownloadsFolder\UPMetroSkin.zip" -Force
-Remove-Item -LiteralPath "$DownloadsFolder\Metro\metro-for-steam-4.4\.gitattributes", "$DownloadsFolder\Metro\metro-for-steam-4.4\.gitignore" -Force
-# Remove-Item -LiteralPath "$DownloadsFolder\Metro\UPMetroSkin-master" -Recurse -Force
+Remove-Item -LiteralPath "$DownloadsFolder\Metro\metro-for-steam-4.4\.gitattributes", "$DownloadsFolder\Metro\metro-for-steam-4.4\.gitignore", "$DownloadsFolder\Metro\UPMetroSkin-master" -Recurse -Force
 
 Get-ChildItem -LiteralPath "$DownloadsFolder\Metro\metro-for-steam-4.4" -Recurse -Force | Move-Item -Destination "$DownloadsFolder\Metro" -Force
 Remove-Item -LiteralPath "$DownloadsFolder\Metro\metro-for-steam-4.4" -Force
@@ -162,11 +158,17 @@ $Parameters = @{
 }
 Invoke-WebRequest @Parameters
 
-if (Test-Path -Path ${env:ProgramFiles(x86)}\Steam)
+if (Test-Path -Path "${env:ProgramFiles(x86)}\Steam")
 {
-	Move-Item -Path "$DownloadsFolder\Metro" -Destination "${env:ProgramFiles(x86)}\Steam\Skins" -Force
-}
-else
-{
-	Write-Warning -Message "No Steam installed"
+	if (-not (Test-Path -Path "${env:ProgramFiles(x86)}\Steam\Skins"))
+	{
+		New-Item -Path "${env:ProgramFiles(x86)}\Steam\Skins" -ItemType Directory -Force
+	}
+
+	if (Test-Path -Path "${env:ProgramFiles(x86)}\Steam\Skins\Metro")
+	{
+		Remove-Item -Path "${env:ProgramFiles(x86)}\Steam\Skins\Metro" -Recurse -Force
+	}
+
+	Move-Item -Path "C:\Users\test\Downloads\Metro" -Destination "${env:ProgramFiles(x86)}\Steam\Skins\Metro" -Force
 }
