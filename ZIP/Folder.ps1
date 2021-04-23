e1a6a6ad81c6f2028abe135562285d512b473340
Dmitry Nefedov <nefedovds@ya.ru>
2021-04-23 10:41:22 +0300

    Update Folder.ps1

--- ZIP/Folder.ps1
+++ ZIP/Folder.ps1
@@ -8,8 +8,11 @@
 	.Parameter Destination
 	Where to expand folder
 
-	.Parameter File
+	.Parameter Folder
 	Assign the folder to expand to
+
+	.Parameter Exclude
+	Exclude files from expanding
 
 	.Example
 	ExtractZIPFolder -Source "D:\Folder\File.zip" -Destination "D:\Folder" -File "Folder1/Folder2"
@@ -26,13 +29,16 @@ function ExtractZIPFolder
 		$Destination,
 
 		[string]
-		$Folder
+		$Folder,
+
+		[string[]]
+		$Exclude
 	)
 
 	Add-Type -Assembly System.IO.Compression.FileSystem
 
 	$ZIP = [IO.Compression.ZipFile]::OpenRead($Source)
-	$ZIP.Entries | Where-Object -FilterScript {$_.FullName -like "$($Folder)/*.*"} | ForEach-Object -Process {
+	$ZIP.Entries | Where-Object -FilterScript {($_.FullName -like "$($Folder)/*.*")  -and ($Exclude -notcontains $_.Name)} | ForEach-Object -Process {
 		$File   = Join-Path -Path $Destination -ChildPath $_.FullName
 		$Parent = Split-Path -Path $File -Parent
 
@@ -51,5 +57,6 @@ $Parameters = @{
 	Source      = "D:\Folder\File.zip"
 	Destination = "D:\Folder"
 	Folder      = "Folder1/Folder2"
+	Exclude     = @("file1.ext", "file2.ext")
 }
 ExtractZIPFolder @Parameters
