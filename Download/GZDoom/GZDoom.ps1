46f7c0852bedf8ffab0cab3377f09c52e0cb4abc
Dmitry Nefedov <nefedovds@ya.ru>
2021-05-07 11:03:11 +0300

    Update GZDoom.ps1

--- Download/GZDoom/GZDoom.ps1
+++ Download/GZDoom/GZDoom.ps1
@@ -1,5 +1,5 @@
-$Tag = ((Invoke-RestMethod -Uri "https://api.github.com/repos/coelckers/gzdoom/releases" -UseBasicParsing) | Where-Object -FilterScript {$_.prerelease -eq $false}).tag_name[0]
-$URL = (((Invoke-WebRequest -Uri "https://api.github.com/repos/coelckers/gzdoom/releases" -UseBasicParsing | ConvertFrom-Json) | Where-Object -FilterScript {$_.prerelease -eq $false} | Where-Object -FilterScript {$_.tag_name -eq $Tag}).assets | Where-Object -FilterScript {$_.browser_download_url -like "*Windows-64bit.zip"}).browser_download_url
+$Tag = (Invoke-RestMethod -Uri "https://api.github.com/repos/coelckers/gzdoom/releases/latest").tag_name
+$URL = ((Invoke-RestMethod -Uri "https://api.github.com/repos/coelckers/gzdoom/releases/latest").assets | Where-Object -FilterScript {$_.browser_download_url -like "*Windows-64bit.zip"}).browser_download_url
 
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
 $Parameters = @{
