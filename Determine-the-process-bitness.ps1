08dcc4029fc0fe72c021a7ce6ebfcfc8249e64ec
Dmitry Nefedov <nefedovds@ya.ru>
2021-07-16 12:03:02 +0300

    Update and rename Determine process bitness.ps1 to Determine-the-process-bitness.ps1

---
+++ Determine-the-process-bitness.ps1
@@ -0,0 +1,43 @@
+# https://rkeithhill.wordpress.com/2014/04/28/how-to-determine-if-a-process-is-32-or-64-bit/
+
+function ProcessBitness
+{
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		[string]
+		$ProcessName
+	)
+
+	$Signature = @{
+		Namespace = "Kernel32"
+		Name = "Bitness"
+		Language = "CSharp"
+		MemberDefinition = @"
+[DllImport("kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi)]
+[return: MarshalAs(UnmanagedType.Bool)]
+public static extern bool IsWow64Process(
+	[In] System.IntPtr hProcess,
+	[Out, MarshalAs(UnmanagedType.Bool)] out bool wow64Process);
+"@
+	}
+	if (-not ("Kernel32.Bitness" -as [type]))
+	{
+		Add-Type @Signature
+	}
+
+	Get-Process -Id (Get-Process -Name $ProcessName).id | ForEach-Object -Process {
+		$is32Bit = [int]0
+		if ([Kernel32.Bitness]::IsWow64Process($_.Handle, [ref]$is32Bit))
+		{
+			if ($is32Bit)
+			{
+				"$($_.Name) is 32-bit"
+			}
+			else
+			{
+				"$($_.Name) is 64-bit"
+			}
+		}
+	}
+}
