8488e3897aa2cd2bad86ab5cfa4781d2d7a6c7a7
Dmitry Nefedov <nefedovds@ya.ru>
2021-08-21 21:57:04 +0300

    Update Firefox_Setup.exe.ps1

--- Download/Firefox_Setup.exe.ps1
+++ Download/Firefox_Setup.exe.ps1
@@ -5,8 +5,8 @@ $LatestStableVersion = (Invoke-WebRequest -Uri "https://product-details.mozilla.
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
 $Language = (Get-WinSystemLocale).Parent.Name
 $Parameters = @{
-	Uri = "https://ftp.mozilla.org/pub/firefox/releases/$($LatestStableVersion)/win64-EME-free/$($Language)/Firefox%20Setup%20$($LatestStableVersion).exe"
-	OutFile = "$DownloadsFolder\Firefox Setup $($LatestStableVersion).exe"
+	Uri = "https://ftp.mozilla.org/pub/firefox/releases/$LatestStableVersion/win64-EME-free/$Language/Firefox%20Setup%20$LatestStableVersion.exe"
+	OutFile = "$DownloadsFolder\Firefox Setup $LatestStableVersion.exe"
 	Verbose = [switch]::Present
 }
 Invoke-WebRequest @Parameters
@@ -14,7 +14,7 @@ Invoke-WebRequest @Parameters
 # Extracting Firefox.exe to the "Firefox Setup xx" folder
 # https://firefox-source-docs.mozilla.org/browser/installer/windows/installer/FullConfig.html
 # Don't paste quotes after /ExtractDir even if a path contains spaces
-Start-Process -FilePath "$DownloadsFolder\Firefox Setup $($LatestStableVersion).exe" -ArgumentList "/ExtractDir=$DownloadsFolder\Firefox Setup $($LatestStableVersion)" -Wait
+Start-Process -FilePath "$DownloadsFolder\Firefox Setup $LatestStableVersion.exe" -ArgumentList "/ExtractDir=$DownloadsFolder\Firefox Setup $LatestStableVersion" -Wait
 
 $Setupini = @"
 [Install]
@@ -26,10 +26,10 @@ PreventRebootRequired=false
 OptionalExtensions=true
 RegisterDefaultAgent=false
 "@
-Set-Content -Path "$DownloadsFolder\Firefox Setup $($LatestStableVersion)\setup.ini" -Value $Setupini -Encoding Default -Force
+Set-Content -Path "$DownloadsFolder\Firefox Setup $LatestStableVersion\setup.ini" -Value $Setupini -Encoding Default -Force
 
-# 
-$Setupini = @'
-"%~dp0setup.exe" /INI=$($DownloadsFolder\Firefox Setup $($LatestStableVersion)\setup.ini)
-'@
-Set-Content -Path "$DownloadsFolder\Firefox Setup $($LatestStableVersion)\setup.cmd" -Value $Setupini -Encoding Default -Force
+# Create a batch file
+$Setupini = @"
+"%~dp0setup.exe" /INI="%~dp0setup.ini"
+"@
+Set-Content -Path "$DownloadsFolder\Firefox Setup $LatestStableVersion\setup.cmd" -Value $Setupini -Encoding Default -Force
