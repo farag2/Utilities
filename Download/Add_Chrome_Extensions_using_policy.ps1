14c3d1e1b7c59e8423a4ad969e80799db61cf7b9
Dmitry Nefedov <nefedovds@ya.ru>
2021-08-22 20:01:22 +0300

    Create Add_Chrome_Extensions_using_policy.ps1

---
+++ Download/Add_Chrome_Extensions_using_policy.ps1
@@ -0,0 +1,75 @@
+<#
+	.SYNOPSIS
+	Install Chrome Extensions using the registry policy
+
+	.PARAMETER ExtensionID
+	String value of an extension ID taken from the Chrome Web Store URL for the extension
+
+	.EXAMPLE
+	New-ChromeExtension -ExtensionID @("cjpalhdlnbpafiamejdnhcphjbkeiagm") -Hive HKLM -Verbose
+
+	.NOTES
+	if you remove the HKLM:\Software\Policies\Google\Chrome\ExtensionInstallForcelist key all extensions will be uninstalled
+#>
+function New-ChromeExtension
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory = $true)]
+		[string[]]
+		$ExtensionIDs,
+
+		[Parameter(Mandatory = $true)]
+		[ValidateSet("HKLM", "HKCU")]
+		[string]
+		$Hive
+	)
+
+	foreach ($ExtensionID in $ExtensionIDs)
+	{
+		switch ($Hive)
+		{
+			"HKLM"
+			{
+				if (-not (Test-Path -Path HKLM:\Software\Policies\Google\Chrome\ExtensionInstallForcelist))
+				{
+					New-Item -Path HKLM:\Software\Policies\Google\Chrome\ExtensionInstallForcelist -Force
+					[int]$Count = 0
+				}
+				else
+				{
+					[int]$Count = (Get-Item -Path HKLM:\Software\Policies\Google\Chrome\ExtensionInstallForcelist).Count
+				}
+			}
+			"HKCU"
+			{
+				if (-not (Test-Path -Path HKCU:\Software\Policies\Google\Chrome\ExtensionInstallForcelist))
+				{
+					New-Item -Path HKCU:\Software\Policies\Google\Chrome\ExtensionInstallForcelist -Force
+					[int]$Count = 0
+				}
+				else
+				{
+					[int]$Count = (Get-Item -Path HKCU:\Software\Policies\Google\Chrome\ExtensionInstallForcelist).Count
+				}
+			}
+		}
+
+		$Name = $Count + 1
+		$Value = "$ExtensionID;https://clients2.google.com/service/update2/crx"
+
+		switch ($Hive)
+		{
+			"HKLM"
+			{
+				New-ItemProperty -Path HKLM:\Software\Policies\Google\Chrome\ExtensionInstallForcelist -Name $Name -PropertyType String -Value $Value -Force
+			}
+			"HKCU"
+			{
+				New-ItemProperty -Path HKCU:\Software\Policies\Google\Chrome\ExtensionInstallForcelist -Name $Name -PropertyType String -Value $Value -Force
+			}
+		}
+	}
+}
+New-ChromeExtension -ExtensionID @("cjpalhdlnbpafiamejdnhcphjbkeiagm") -Hive HKLM -Verbose
