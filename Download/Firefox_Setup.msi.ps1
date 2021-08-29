14d5f02a74468226bec4589096a4b255f097046c
Dmitry Nefedov <nefedovds@ya.ru>
2021-08-29 23:32:52 +0300

    Update Firefox_Setup.msi.ps1

--- Download/Firefox_Setup.msi.ps1
+++ Download/Firefox_Setup.msi.ps1
@@ -1,29 +1,62 @@
 # Downloading Firefox Setup xx.msi
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$LatestStableVersion = (Invoke-WebRequest -Uri "https://product-details.mozilla.org/1.0/firefox_versions.json" | ConvertFrom-Json).LATEST_FIREFOX_VERSION
+# Mozilla shortens some locale codes to 2 or 3 letters, so we cannot use (Get-WinSystemLocale).Name for every language
+$Languages = @(
+	"ach", "af", "an", "ar", "ast",
+	"az", "be", "bg", "bn", "br",
+	"bs", "ca", "cak", "cs", "cy",
+	"da", "de", "dsb", "el", "en-CA",
+	"en-GB", "en-US", "eo", "es-AR", "es-CL",
+	"es-ES", "es-MX", "et", "eu", "fa",
+	"ff", "fi", "fr", "fy-NL", "ga-IE",
+	"gd", "gl", "gn", "gu-IN", "he",
+	"hi-IN", "hr", "hsb", "hu", "hy-AM",
+	"ia", "id", "is", "it", "ja",
+	"ka", "kab", "kk", "km", "kn",
+	"ko", "lij", "lt", "lv", "mk",
+	"mr", "ms", "my", "nb-NO", "ne-NP",
+	"nl", "nn-NO", "oc", "pa-IN", "pl",
+	"pt-BR", "pt-PT", "rm", "ro", "ru",
+	"si", "sk", "sl", "son", "sq",
+	"sr", "sv-SE", "ta", "te", "th",
+	"tr", "uk", "ur", "uz", "vi",
+	"xh", "zh-CN", "zh-TW"
+)
+if ((Get-WinSystemLocale).Name -in $Languages)
+{
+	$Language = (Get-WinSystemLocale).Name
+}
+else
+{
+	$Language = (Get-WinSystemLocale).Parent.Name
+}
+
+$LatestStableVersion = (Invoke-RestMethod -Uri "https://product-details.mozilla.org/1.0/firefox_versions.json" -UseBasicParsing).LATEST_FIREFOX_VERSION
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
-$Language = (Get-WinSystemLocale).Parent.Name # ru
 $Parameters = @{
-	Uri = "https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=win64&lang=$Language"
-	OutFile = "$DownloadsFolder\Firefox Setup $($LatestStableVersion).msi"
-	Verbose = [switch]::Present
+	Uri             = "https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=win64&lang=$Language"
+	OutFile         = "$DownloadsFolder\Firefox Setup $LatestStableVersion.msi"
+	UseBasicParsing = $true
+	Verbose         = $true
 }
 Invoke-WebRequest @Parameters
 
 # Extracting Firefox.msi to the "Firefox Setup xx" folder
 # https://support.mozilla.org/kb/deploy-firefox-msi-installers
-Start-Process -FilePath "$DownloadsFolder\Firefox Setup $($LatestStableVersion).msi" -ArgumentList "EXTRACT_DIR=`"$DownloadsFolder\Firefox Setup $($LatestStableVersion)`"" -Wait
+Start-Process -FilePath "$DownloadsFolder\Firefox Setup $LatestStableVersion.msi" -ArgumentList "EXTRACT_DIR=`"$DownloadsFolder\Firefox Setup $LatestStableVersion`"" -Wait
 
-Remove-Item -Path "$DownloadsFolder\Firefox Setup $($LatestStableVersion)\postSigningData" -Force
+Remove-Item -Path "$DownloadsFolder\Firefox Setup $LatestStableVersion\postSigningData" -Force
 
 # It isn’t possible to create taskbar pins on Windows 10 and later
 $Arguments = @(
-    "DESKTOP_SHORTCUT=false",
-    "START_MENU_SHORTCUT=true",
-    "INSTALL_MAINTENANCE_SERVICE=true",
-    "PREVENT_REBOOT_REQUIRED=false",
-    "OPTIONAL_EXTENSIONS=true"
+	"DESKTOP_SHORTCUT=false",
+	"START_MENU_SHORTCUT=true",
+	"INSTALL_MAINTENANCE_SERVICE=true",
+	"PREVENT_REBOOT_REQUIRED=false",
+	"OPTIONAL_EXTENSIONS=true"
 )
 
 Start-Process -FilePath "$DownloadsFolder\Firefox Setup $LatestStableVersion.msi" -ArgumentList $Arguments -Wait
+
+Remove-Item -Path "$DownloadsFolder\Firefox Setup $LatestStableVersion.msi" -Force
