9862a81a77a4bc4fc83daaa9700f0c2f335aabca
Dmitry Nefedov <nefedovds@ya.ru>
2021-08-29 23:23:37 +0300

    Update Firefox_Setup.exe.ps1

--- Download/Firefox_Setup.exe.ps1
+++ Download/Firefox_Setup.exe.ps1
@@ -1,13 +1,44 @@
 # Downloading Firefox Setup.exe
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$LatestStableVersion = (Invoke-WebRequest -Uri "https://product-details.mozilla.org/1.0/firefox_versions.json" | ConvertFrom-Json).LATEST_FIREFOX_VERSION
+# Mozilla shortens some locale codes to 2 or 3 letters, so we cannot use (Get-WinSystemLocale).Name for every language
+$Languages = @(
+	"ach", "af", "an", "ar", "ast",
+	"az", "be", "bg", "bn", "br",
+	"bs", "ca", "cak", "cs", "cy",
+	"da", "de", "dsb", "el", "en-CA",
+	"en-GB", "en-US", "eo", "es-AR", "es-CL",
+	"es-ES", "es-MX", "et", "eu", "fa",
+	"ff", "fi", "fr", "fy-NL", "ga-IE",
+	"gd", "gl", "gn", "gu-IN", "he",
+	"hi-IN", "hr", "hsb", "hu", "hy-AM",
+	"ia", "id", "is", "it", "ja",
+	"ka", "kab", "kk", "km", "kn",
+	"ko", "lij", "lt", "lv", "mk",
+	"mr", "ms", "my", "nb-NO", "ne-NP",
+	"nl", "nn-NO", "oc", "pa-IN", "pl",
+	"pt-BR", "pt-PT", "rm", "ro", "ru",
+	"si", "sk", "sl", "son", "sq",
+	"sr", "sv-SE", "ta", "te", "th",
+	"tr", "uk", "ur", "uz", "vi",
+	"xh", "zh-CN", "zh-TW"
+)
+if ((Get-WinSystemLocale).Name -in $Languages)
+{
+	$Language = (Get-WinSystemLocale).Name
+}
+else
+{
+	$Language = (Get-WinSystemLocale).Parent.Name
+}
+
+$LatestStableVersion = (Invoke-RestMethod -Uri "https://product-details.mozilla.org/1.0/firefox_versions.json" -UseBasicParsing).LATEST_FIREFOX_VERSION
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
-$Language = (Get-WinSystemLocale).Parent.Name # ru
 $Parameters = @{
-	Uri = "https://ftp.mozilla.org/pub/firefox/releases/$LatestStableVersion/win64-EME-free/$Language/Firefox%20Setup%20$LatestStableVersion.exe"
-	OutFile = "$DownloadsFolder\Firefox Setup $LatestStableVersion.exe"
-	Verbose = [switch]::Present
+	Uri             = "https://ftp.mozilla.org/pub/firefox/releases/$LatestStableVersion/win64-EME-free/$Language/Firefox%20Setup%20$LatestStableVersion.exe"
+	OutFile         = "$DownloadsFolder\Firefox Setup $LatestStableVersion.exe"
+	UseBasicParsing = $true
+	Verbose         = $true
 }
 Invoke-WebRequest @Parameters
 
@@ -29,7 +60,9 @@ RegisterDefaultAgent=false
 Set-Content -Path "$DownloadsFolder\Firefox Setup $LatestStableVersion\setup.ini" -Value $Setupini -Encoding Default -Force
 
 # Create a batch file
-$Setupini = @"
-"%~dp0setup.exe" /INI="%~dp0setup.ini"
+$Setupcmd = @"
+`"%~dp0setup.exe`" /INI=`"%~dp0setup.ini`"
 "@
-Set-Content -Path "$DownloadsFolder\Firefox Setup $LatestStableVersion\setup.cmd" -Value $Setupini -Encoding Default -Force
+Set-Content -Path "$DownloadsFolder\Firefox Setup $LatestStableVersion\setup.cmd" -Value $Setupcmd -Encoding Default -Force
+
+Remove-Item -Path "$DownloadsFolder\Firefox Setup $LatestStableVersion.exe" -Force
