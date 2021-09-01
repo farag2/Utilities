50decbcac30ac00b9ff2bca53937c89a9afd9b88
Dmitry Nefedov <nefedovds@ya.ru>
2021-09-01 22:25:21 +0300

    Update AlterID.ps1

--- TeamViewer/Scripts/AlterID.ps1
+++ TeamViewer/Scripts/AlterID.ps1
@@ -2,11 +2,17 @@
 
 powershell.exe -NoProfile -NoLogo -Command Start-Process powershell -WindowStyle Hidden -ArgumentList "{
 	Start-Sleep -Seconds 3
+
 	TAKEOWN /F '$PSScriptRoot\..\rolloutfile.tv13'
 	ICACLS '$PSScriptRoot\..\rolloutfile.tv13' --% /grant:r %USERNAME%:F
-	Remove-Item -Path '$PSScriptRoot\..\rolloutfile.tv13' -Force
-	Remove-Item -Path HKCU:\Software\TeamViewer -Recurse -Force -ErrorAction Ignore
-	Remove-Item $env:LOCALAPPDATA\TeamViewer -Recurse -Force
-	Remove-Item $env:APPDATA\TeamViewer -Recurse -Force
+
+	$Lefovers = @(
+		'$PSScriptRoot\..\rolloutfile.tv13',
+		$env:LOCALAPPDATA\TeamViewer,
+		$env:APPDATA\TeamViewer,
+		HKCU:\Software\TeamViewer
+	)
+	Remove-Item -Path $Lefovers -Recurse -Force
+
 	exit
 }" -Verb RunAs
