9abe66aa96638d2710a08bcf0acf564c635860fa
Dmitry Nefedov <nefedovds@ya.ru>
2021-09-01 22:01:19 +0300

    Update TeamViewer.ps1

--- TeamViewer/Scripts/TeamViewer.ps1
+++ TeamViewer/Scripts/TeamViewer.ps1
@@ -10,8 +10,6 @@ Start-Process -FilePath "$PSScriptRoot\..\TeamViewer.exe" -Wait
 Start-Sleep -Seconds 3
 TAKEOWN /F "$PSScriptRoot\..\rolloutfile.tv13"
 ICACLS "$PSScriptRoot\..\rolloutfile.tv13" --% /grant:r %USERNAME%:F
-Remove-Item -Path "$PSScriptRoot\..\rolloutfile.tv13" -Force
+Remove-Item -Path "$PSScriptRoot\..\rolloutfile.tv13", $env:LOCALAPPDATA\TeamViewer, $env:APPDATA\TeamViewer -Recurse -Force
 Remove-Item -Path HKCU:\Software\TeamViewer -Recurse -Force
-Remove-Item $env:LOCALAPPDATA\TeamViewer -Recurse -Force
-Remove-Item $env:APPDATA\TeamViewer -Recurse -Force
 exit
