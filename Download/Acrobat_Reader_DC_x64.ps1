35e3b0d4d27a315c96080dcbd0346ebd65227ff1
Dmitry Nefedov <nefedovds@ya.ru>
2021-09-14 20:50:49 +0300

    Update Acrobat_Reader_DC_x64.ps1

--- Download/Acrobat_Reader_DC_x64.ps1
+++ Download/Acrobat_Reader_DC_x64.ps1
@@ -3,6 +3,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # https://www.adobe.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/index.html
+# Trim() needed because the current_version.txt files has a blank line
 $LatestVersion = (Invoke-RestMethod -Uri "https://armmf.adobe.com/arm-manifests/mac/AcrobatDC/reader/current_version.txt").Replace(".","").Trim()
 $Locale = $PSUICulture.Replace("-", "_")
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
