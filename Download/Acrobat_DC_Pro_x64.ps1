a7d84cb3f6b42f6b4ebc50d9971f71b13dcfae97
Dmitry Nefedov <nefedovds@ya.ru>
2021-09-15 09:45:30 +0300

    Update Acrobat_DC_Pro_x64.ps1

--- Download/Acrobat_DC_Pro_x64.ps1
+++ Download/Acrobat_DC_Pro_x64.ps1
@@ -133,7 +133,7 @@ $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows
 # https://www.adobe.com/devnet-docs/acrobatetk/tools/ReleaseNotesDC/index.html
 if (Test-Path -Path "$DownloadsFolder\Adobe Acrobat\AcrobatDCx64Upd*.msp")
 {
-	$LatestPatchVersion = (Invoke-RestMethod -Uri "https://armmf.adobe.com/arm-manifests/mac/AcrobatDC/acrobat/current_version.txt").Replace(".","")
+	$LatestPatchVersion = (Invoke-RestMethod -Uri "https://armmf.adobe.com/arm-manifests/mac/AcrobatDC/acrobat/current_version.txt").Replace(".","").Trim()
 	# Get the bare patch number to compare with the latest one
 	$CurrentPatchVersion = (Split-Path -Path (Get-Item -Path "$DownloadsFolder\Adobe Acrobat\AcrobatDCx64Upd*.msp").FullName -Leaf).Replace(".msp","").Replace("AcrobatDCx64Upd","")
 	if ($CurrentPatchVersion -lt $LatestPatchVersion)
