8d85092b748cceea228a61b9117ee577e478f62a
Dmitry Nefedov <nefedovds@ya.ru>
2021-12-06 02:05:55 +0300

    Update MetroSteam2.ps1

--- Steam/MetroSteam2.ps1
+++ Steam/MetroSteam2.ps1
@@ -63,6 +63,7 @@ function ExtractZIPFolder
 	Add-Type -Assembly System.IO.Compression.FileSystem
 
 	$ZIP = [IO.Compression.ZipFile]::OpenRead($Source)
+
 	$ZIP.Entries | Where-Object -FilterScript {($_.FullName -like "$($Folder)/*.*") -and ($Exclude -notcontains $_.Name)} | ForEach-Object -Process {
 		$File   = Join-Path -Path $Destination -ChildPath $_.FullName
 		$Parent = Split-Path -Path $File -Parent
@@ -86,20 +87,130 @@ $Parameters = @{
 }
 ExtractZIPFolder @Parameters
 
+<#
+	Expanding the archive using IO.Compression.ZipFileExtensions due to it's much faster
+	IO.Compression.ZipFileExtensions cannot work with paths that contain excape chars like "[",
+	and the same time we have to use $_.FullName -like (-match won't work too, even with escaped \[Install First\]), so the only way out is to place asterix
+
+	So we cannot just expand (thanks the developer for [] in the path) the archive and overwrite all files with new ones from the patch
+#>
 $Parameters = @{
 	Source      = "$DownloadsFolder\UPMetroSkin.zip"
 	Destination = "$DownloadsFolder"
-	Folder      = "UPMetroSkin-master\Unofficial 4.x Patch\Main Files [Install First]"
+	Folder      = "UPMetroSkin-master/Unofficial 4.x Patch/Main Files*" #UPMetroSkin-master/Unofficial 4.x Patch/Main Files [Install First]
 }
 ExtractZIPFolder @Parameters
 
-Remove-Item -Path "$DownloadsFolder\metro-for-steam.zip", "$DownloadsFolder\UPMetroSkin.zip" -Force
+<#
+	.SYNOPSIS
+	Copying files and folders from one folder to another saving folders' structure
+
+	.Parameter Source
+	Source folder to copy content from
+
+	.Parameter Destination
+	Destination folder to copy content to
+
+	.Parameter Include
+	Include files
+
+	.Parameter Exclude
+	Exclude files
+
+	.Parameter Delete
+	Remove all copied items in the source folder recursively
+
+	.Parameter DeleteEmpty
+	Remove empty folders in the source folder recursively
+
+	.Parameter DeleteAll
+	Remove the source folder
+
+	.Example
+	Move-Recursively -Source "D:\FOLDER1" -Destination "d:\Folder2" -Include '*.pdf', '*.txt' -Exclude '*_out.*' -Delete
+
+	.Link
+	https://forum.ru-board.com/topic.cgi?forum=62&topic=30859&start=3600#4
+#>
+function Move-Recursively
+{
+	[CmdletBinding()]
+	param
+	(
+		[string]
+		$Source,
+
+		[string]
+		$Destination,
+
+		[string[]]
+		$Include = "*.*",
+
+		[string[]]
+		$Exclude = "",
+
+		[switch]
+		$Delete,
+
+		[switch]
+		$DeleteEmpty,
+
+		[switch]
+		$DeleteAll
+	)
+
+	# Copying files saving folders' structure
+	# -Include & -Exclude work with -LiteralPath only in PowerShell 7
+	Get-ChildItem -LiteralPath $Source -Include $Include -Exclude $Exclude -Recurse -Force | Copy-Item -Destination {
+		$Folder = Split-Path -Path $_.FullName.Replace($Source, $Destination)
+
+		if (-not (Test-Path -LiteralPath $Folder))
+		{
+			New-Item -Path $Folder -ItemType Directory -Force
+		}
+		else
+		{
+			$Folder
+		}
+	} -Force
+
+	# Removing all copied items
+	if ($Delete)
+	{
+		Get-ChildItem -LiteralPath $Source -Include $Include -Exclude $Exclude -Recurse -File -Force | Remove-Item -Recurse -Force
+	}
+
+	# Removing empty folders
+	if ($DeleteEmpty)
+	{
+		Get-ChildItem -LiteralPath $Source -Recurse -Directory -Force | Sort-Object {$_.FullName.Length} -Descending | ForEach-Object -Process {
+			if ($null -eq (Get-ChildItem -LiteralPath $_.FullName -Recurse -Force))
+			{
+				Remove-Item -LiteralPath $_.FullName -Force
+			}
+		}
+	}
+
+	# Remove the source folder
+	if ($DeleteAll)
+	{
+		Remove-Item -LiteralPath $Source -Recurse -Force
+	}
+}
+$Parameters = @{
+	Source      = "$DownloadsFolder\UPMetroSkin-master\Unofficial 4.x Patch\Main Files [Install First]"
+	Destination = "$DownloadsFolder\metro-for-steam-4.4"
+	DeleteAll   = $true
+}
+Move-Recursively @Parameters
+
+Remove-Item -Path "$DownloadsFolder\metro-for-steam.zip", "$DownloadsFolder\UPMetroSkin.zip", "$DownloadsFolder\UPMetroSkin-master" -Recurse -Force
 
 Rename-Item -Path "$DownloadsFolder\metro-for-steam-4.4" -NewName "Metro" -Force
 
 # Custom menu
 $Parameters = @{
-	Uri             = "https://github.com/farag2/Utilities/blob/master/Steam/steam.menu"
+	Uri             = "https://raw.githubusercontent.com/farag2/Utilities/master/Steam/steam.menu"
 	OutFile         = "$DownloadsFolder\Metro\resource\menus\steam.menu"
 	UseBasicParsing = $true
 	Verbose         = $true
@@ -118,5 +229,9 @@ if (Test-Path -Path "${env:ProgramFiles(x86)}\Steam")
 		Remove-Item -Path "${env:ProgramFiles(x86)}\Steam\Skins\Metro" -Recurse -Force
 	}
 
+	Get-Process -Name steam | Stop-Process -Force -ErrorAction Ignore
+	Remove-Item -Path "${env:ProgramFiles(x86)}\Steam\Skins\Metro" -Recurse -Force -ErrorAction Ignore
+	Copy-Item -Path "$DownloadsFolder\Metro\resource\menus\steam.menu" -Destination "$DownloadsFolder\Metro\resource\menus\steam_original.menu" -Force
 	Move-Item -Path "$DownloadsFolder\Metro" -Destination "${env:ProgramFiles(x86)}\Steam\Skins\Metro" -Force
+	Invoke-Item -Path "${env:ProgramFiles(x86)}\Steam\Skins\Metro\resource\menus"
 }
