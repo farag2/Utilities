9c61bccb43dd9c81b3228f736718041480d3f573
Dmitry Nefedov <nefedovds@ya.ru>
2022-01-09 16:02:18 +0300

    Update GZDoom.ps1

--- Download/GZDoom/GZDoom.ps1
+++ Download/GZDoom/GZDoom.ps1
@@ -1,60 +1,57 @@
-$Tag = (Invoke-RestMethod -Uri "https://api.github.com/repos/coelckers/gzdoom/releases/latest").tag_name
 $URL = ((Invoke-RestMethod -Uri "https://api.github.com/repos/coelckers/gzdoom/releases/latest").assets | Where-Object -FilterScript {$_.browser_download_url -like "*Windows-64bit.zip"}).browser_download_url
 
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
 $Parameters = @{
-	Uri = $URL
-	OutFile = "$DownloadsFolder\GZDoom_$Tag.zip"
-	Verbose = [switch]::Present
+	Uri     = $URL
+	OutFile = "$DownloadsFolder\GZDoom.zip"
+	Verbose = $true
 }
 Invoke-WebRequest @Parameters
 
 $Parameters = @{
-	Path = "$DownloadsFolder\GZDoom_$Tag.zip"
-	DestinationPath = "$DownloadsFolder\GZDoom_$Tag"
-	Force = [switch]::Present
-	Verbose = [switch]::Present
+	Path            = "$DownloadsFolder\GZDoom.zip"
+	DestinationPath = "$DownloadsFolder\GZDoom"
+	Force           = $true
+	Verbose         = $true
 }
-if (Test-Path "$DownloadsFolder\GZDoom_$Tag.zip")
-{
-    Expand-Archive @Parameters
-}
+Expand-Archive @Parameters
 
-Remove-Item -Path "$DownloadsFolder\GZDoom_$Tag.zip" -Force
-Remove-Item -Path "$DownloadsFolder\GZDoom_$Tag\fm_banks" -Recurse -Force
-Remove-Item -Path "D:\Downloads\GZDoom_g4.5.0\licenses.zip" -Force
+$Parameters = @(
+	"$DownloadsFolder\GZDoom.zip",
+    "$DownloadsFolder\GZDoom\fm_banks",
+    "$DownloadsFolder\GZDoom\licenses.zip"
+)
+Remove-Item -Path $Parameters -Recurse -Force
 
 $Parameters = @{
-	Uri = "https://github.com/farag2/Utilities/blob/master/Download/GZDoom/gzdoom.ini"
-	OutFile = "$DownloadsFolder\GZDoom_$Tag\gzdoom.ini"
-	Verbose = [switch]::Present
+	Uri     = "https://raw.githubusercontent.com/farag2/Utilities/master/Download/GZDoom/gzdoom.ini"
+	OutFile = "$DownloadsFolder\GZDoom\gzdoom.ini"
+	Verbose = $true
 }
 Invoke-WebRequest @Parameters
 
-Get-Item -Path "$DownloadsFolder\GZDoom_$Tag\gzdoom.ini" -Force | Rename-Item -NewName "gzdoom-$env:USERNAME.ini" -Force
+Get-Item -Path "$DownloadsFolder\GZDoom\gzdoom.ini" -Force | Rename-Item -NewName "gzdoom-$env:USERNAME.ini" -Force
 
 $Parameters = @{
-	Uri = "https://github.com/farag2/Utilities/blob/master/Download/GZDoom/_pb.cmd"
-	OutFile = "$DownloadsFolder\GZDoom_$Tag\_pb.cmd"
-	Verbose = [switch]::Present
+	Uri     = "https://raw.githubusercontent.com/farag2/Utilities/master/Download/GZDoom/_pb.cmd"
+	OutFile = "$DownloadsFolder\GZDoom\_pb.cmd"
+	Verbose = $true
 }
 Invoke-WebRequest @Parameters
 
 $Parameters = @{
-	Uri = "https://github.com/farag2/Utilities/blob/master/Download/GZDoom/_bd.cmd"
-	OutFile = "$DownloadsFolder\GZDoom_$Tag\_bd.cmd"
-	Verbose = [switch]::Present
+	Uri     = "https://raw.githubusercontent.com/farag2/Utilities/master/Download/GZDoom/_bd.cmd"
+	OutFile = "$DownloadsFolder\GZDoom\_bd.cmd"
+	Verbose = $true
 }
 Invoke-WebRequest @Parameters
 
-<#
 $URLs = @(
-	# "https://github.com/coelckers/gzdoom/releases/",
+	"https://github.com/coelckers/gzdoom/releases/",
 	"https://www.moddb.com/downloads/start/95667",
 	"http://iddqd.ru/levels?find=Doom%202:%20Hell%20on%20Earth"
 )
-foreach($URL in $URLs)
+foreach ($URL in $URLs)
 {
 	Start-Process -FilePath $URL
 }
-#>
