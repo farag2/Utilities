c843994ad52b78c07199f6790378be9b2563c30e
Dmitry Nefedov <nefedovds@ya.ru>
2022-03-20 23:04:13 +0300

    Update Acrobat_Pro_DC_x86.ps1

--- Download/Acrobat_Pro_DC_x86.ps1
+++ Download/Acrobat_Pro_DC_x86.ps1
@@ -174,10 +174,7 @@ $CmdLine = @(
 	"REMOVE_PREVIOUS=YES",
 	"IGNOREVCRT64=1",
 	"EULA_ACCEPT=YES",
-	"DISABLE_PDFMAKER=YES",
-	"DISABLEDESKTOPSHORTCUT=2",
-	# Install updates automatically
-	"UPDATE_MODE=3"
+	"DISABLE_PDFMAKER=YES"
 )
 
 $LCID = (Get-WinSystemLocale).LCID
