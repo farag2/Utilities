c6ba865ba9681299c613d42a6ddaf89bbeb5d064
Dmitry Nefedov <nefedovds@ya.ru>
2022-03-29 00:12:01 +0300

    Update JSON.ps1

--- JSON.ps1
+++ JSON.ps1
@@ -104,6 +104,6 @@ $Terminal = Get-Content -Path "D:\Downloads\1.js" -Encoding UTF8 -Force | Conver
 $Terminal | ForEach-Object {$_.Header | Add-Member -MemberType NoteProperty -Name "UA" -Value "" -Force}
 $Terminal | ForEach-Object {$_.Description | Add-Member -MemberType NoteProperty -Name "UA" -Value "" -Force}
 
-ConvertTo-Json -InputObject $Terminal -Depth 4 | Set-Content -Path "D:\Downloads\1.js"-Encoding UTF8 -Force
+ConvertTo-Json -InputObject $Terminal -Depth 4 | Set-Content -Path "D:\Downloads\1.js" -Encoding UTF8 -Force
 # Re-save in the UTF-8 without BOM encoding due to JSON must not has the BOM: https://datatracker.ietf.org/doc/html/rfc8259#section-8.1
 Set-Content -Value (New-Object -TypeName System.Text.UTF8Encoding -ArgumentList $false).GetBytes($(Get-Content -Path "D:\Downloads\1.js" -Raw)) -Encoding Byte -Path "D:\Downloads\1.js" -Force
