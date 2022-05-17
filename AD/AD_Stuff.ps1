f25b141334f78d49f6d40bce43922cf61e8361fa
Dmitry Nefedov <nefedovds@ya.ru>
2022-05-17 21:04:11 +0300

    Update AD_Stuff.ps1

--- AD/AD_Stuff.ps1
+++ AD/AD_Stuff.ps1
@@ -140,3 +140,21 @@ Get-ADGroupMember -Identity group -Server na.domain.com | ForEach-Object -Proces
 
 }
 # $All.Count
+
+# Create a table with userIDs and full path to profiles if profile contains file.ini that weas modified more than half of year ago
+Get-ChildItem -Path \\server\folder$ -Recurse -Force | ForEach-Object -Process {
+        Write-Verbose -Message $_ -Verbose
+
+        # Some profiles doesn't have file.ini
+        if (Test-Path -Path "$($_.FullName)\file.ini")
+        {
+               # Get files if they are old than 180 days (~ half of a year)
+               Get-Item -Path "$($_.FullName)\file.ini" -Force | Where-Object -FilterScript {$_.LastWriteTime -lt (Get-Date).AddDays(-180)} | ForEach-Object -Process {
+                       [PSCustomObject]@{
+                               FullName      = $_.FullName
+                               userID        = Split-Path -Path $_.FullName | Split-Path -Leaf
+                               LastWriteTime = $_.LastWriteTime.ToString("dd.MM.yyyy")
+                       }
+               } | Select-Object -Property FullName, userID, LastWriteTime | Export-Csv -Path "D:\list.csv" -Encoding UTF8 -NoTypeInformation -Delimiter ';' -Append
+        }
+}
