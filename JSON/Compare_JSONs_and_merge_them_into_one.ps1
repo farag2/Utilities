cef9f96d41e91b08d1e768166c08a5840e1db037
Dmitry Nefedov <nefedovds@ya.ru>
2022-06-13 16:16:50 +0300

    Update Compare_JSONs_and_merge_them_into_one.ps1

--- JSON/Compare_JSONs_and_merge_them_into_one.ps1
+++ JSON/Compare_JSONs_and_merge_them_into_one.ps1
@@ -8,7 +8,7 @@ $Parameters = @{
 $Full = Invoke-RestMethod @Parameters
 
 $Parameters = @{
-	Uri             = "https://raw.githubusercontent.com/Sophia-Community/SophiApp/9c300d4d9ac84a3211f40aa74f2fe6dcb6f5345e/SophiApp/SophiApp/Localizations/UIData_TR.json"
+	Uri             = "https://raw.githubusercontent.com/Sophia-Community/SophiApp/master/SophiApp/SophiApp/Localizations/UIData_TR.json"
 	UseBasicParsing = $true
 }
 $Translation = Invoke-RestMethod @Parameters
@@ -35,7 +35,7 @@ $Full | ForEach-Object -Process {
 	}
 }
 
-ConvertTo-Json -InputObject $Full -Depth 4 | Set-Content -Path "D:\3.json" -Encoding UTF8 -Force
+ConvertTo-Json -InputObject $Full -Depth 4 | ForEach-Object -Process {$_.Replace("\u0027", "'")} | Set-Content -Path "D:\3.json" -Encoding UTF8 -Force
 
 # Re-save in the UTF-8 without BOM encoding due to JSON must not has the BOM: https://datatracker.ietf.org/doc/html/rfc8259#section-8.1
 Set-Content -Value (New-Object -TypeName System.Text.UTF8Encoding -ArgumentList $false).GetBytes($(Get-Content -Path "D:\3.json" -Raw)) -Encoding Byte -Path "D:\3.json" -Force
