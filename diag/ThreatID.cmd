415ba873679b2ba195c9e405291268e9f80987b0
Dmitry Nefedov <nefedovds@ya.ru>
2022-07-05 21:32:22 +0300

    Update ThreatID.cmd

--- diag/ThreatID.cmd
+++ diag/ThreatID.cmd
@@ -1,20 +1,11 @@
 set ThreatID= ^
 Write-Host "KMS"; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 225062 -ThreatIDDefaultAction_Actions Allow -Force; ^
 Add-MpPreference -ThreatIDDefaultAction_Ids 2147685180 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 2147714384 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 2147727613 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 2147734094 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 2147743522 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ExclusionPath C:\Windows\KMS -Force; ^
-Add-MpPreference -ExclusionPath $env:SystemRoot\KMSAutoS -Force; ^
-Add-MpPreference -ExclusionPath $env:SystemRoot\System32\SppExtComObjHook.dll -Force; ^
-Add-MpPreference -ExclusionPath $env:SystemRoot\System32\SppExtComObjPatcher.exe -Force; ^
+
 Write-Host "KMS Cleaner"; ^
 Add-MpPreference -ThreatIDDefaultAction_Ids 2147749214 -ThreatIDDefaultAction_Actions Allow -Force; ^
+
 Write-Host "Adobe patch"; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 2147593794 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Write-Host "AutoCAD"; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 2147648291 -ThreatIDDefaultAction_Actions Allow -Force; ^
-Add-MpPreference -ThreatIDDefaultAction_Ids 2147726780 -ThreatIDDefaultAction_Actions Allow -Force;
-START powershell -NoExit -NoLogo -NoProfile -Command %ThreatID%
+Add-MpPreference -ThreatIDDefaultAction_Ids 2147593794 -ThreatIDDefaultAction_Actions Allow -Force;
+
+start powershell -noexit -command %ThreatID%
