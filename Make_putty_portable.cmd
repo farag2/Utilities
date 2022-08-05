8fc2bf1a2d4bcfec584fe6426fffe52cb9062ded
Dmitry Nefedov <nefedovds@ya.ru>
2022-08-05 20:03:34 +0300

    Update Make_putty_portable.cmd

--- Make_putty_portable.cmd
+++ Make_putty_portable.cmd
@@ -1,6 +1,10 @@
 :: https://the.earth.li/~sgtatham/putty/0.58/htmldoc/Chapter4.html#config-file
 
 @ECHO OFF
-regedit /s %~dp0putty.reg
+REG ADD HKCU\Software\SimonTatham\PuTTY\Sessions\VDSina /v HostName /t REG_SZ /d "" /f
+REG ADD HKCU\Software\SimonTatham\PuTTY\SshHostKeys /v "" /t REG_SZ /d "" /f
+
 start /wait %~dp0putty.exe
+
 reg delete HKEY_CURRENT_USER\Software\SimonTatham /f
+DEL %LOCALAPPDATA%\PUTTY.RND /f
