928096f8d7742cab3664425fe475b13957476ef2
Dmitry Nefedov <nefedovds@ya.ru>
2022-09-04 15:10:18 +0300

    Update Search_UWP_Package_in_Store.ps1

--- Search_UWP_Package_in_Store.ps1
+++ Search_UWP_Package_in_Store.ps1
@@ -1,8 +1,6 @@
-#Requires -Version 5.1
-
 <#
 	.SYNOPSIS
-	Search Microsoft Store from PowerShell 
+	Search in Microsoft Store from PowerShell 
 
 	.PARAMETER Query
 
@@ -10,9 +8,6 @@
 	Search-MSStore -Query "terminal"
 
 	.NOTES
-	Originally coded by antidisestablishmentarianism
-	https://dev.to/antidisestablishmentarianism/search-microsoft-store-from-powershell-2bjj
-	
 	https://github.com/ThomasPe/MS-Store-API
 #>
 function Search-MSStore
@@ -25,95 +20,21 @@ function Search-MSStore
 		$Query
 	)
 
-	$Base = "https://storeedgefd.dsx.mp.microsoft.com/v8.0/search?"
-
-	$Parameters = [PSCustomObject]@{
-		market = (Get-UICulture).Name.Split("-")[1]
-		locale = $PSCulture
-		catalogLocales = $PSCulture
-		query = $Query
-		mediaType = "apps"
-		category = "all"
-		moId = "Public"
-		oemId = "LENOVO"
-		scmId = "Lenovo3_Idea"
+	$Keys = [PSCustomObject]@{
+		appVersion   = "22203.1401.0.0"
+		market       = "US"
+		locale       = "en-US"
 		deviceFamily = "windows.desktop"
-		appVersion = "12006.1001.0.0"
-		availableOn = "windows.desktop"
-		maturityRating = "all"
-		cardsEnabled = "true"
-		pzn = "0"
-		pageSize = "25"
-		skipItems = "0"
+		query        = "calculator"
 	}
 
-	[string]$Parameters = (-join ($Parameters.psobject.Properties.Name | ForEach-Object -Process {$_ + "=" + $Parameters.$_ + "&"})).TrimEnd("&")
-
-	$URL = @{
-		Name = "URL"
-		Expression = {"https://www.microsoft.com/store/productId/" + $_.ProductId}
-	}
-	((Invoke-WebRequest -UseBasicParsing -Uri ($Base + $Parameters)).Content | ConvertFrom-Json).Payload.Cards | Select-Object -Property Title, Price, ProductId, $URL
-}
-
-###
-# Another version without [PSCustomObject]
-###
-
-#Requires -Version 7.1
-
-<#
-	.SYNOPSIS
-	Search Microsoft Store from PowerShell 
-
-	.PARAMETER Query
-
-	.EXAMPLE
-	Search-MSStore -Query "terminal"
-
-	.NOTES
-	Originally coded by antidisestablishmentarianism
-	https://dev.to/antidisestablishmentarianism/search-microsoft-store-from-powershell-2bjj
-
-	https://github.com/ThomasPe/MS-Store-API
-#>
-function Search-MSStore
-{
-	[CmdletBinding()]
-	param
-	(
-		[Parameter(Mandatory = $true)]
-		[string]
-		$Query
-	)
-
-	$Base = "https://storeedgefd.dsx.mp.microsoft.com/v8.0/search?"
+	[string]$Keys = (-join ($Keys.psobject.Properties.Name | ForEach-Object -Process {$_ + "=" + $Keys.$_ + "&"})).TrimEnd("&")
 
 	$Parameters = @{
-		market = (Get-UICulture).TwoLetterISOLanguageName
-		locale = $PSCulture
-		catalogLocales = $PSCulture
-		query = $Query
-		mediaType = "apps"
-		category = "all"
-		moId = "Public"
-		oemId = "LENOVO"
-		scmId = "Lenovo3_Idea"
-		deviceFamily = "windows.desktop"
-		appVersion = "12006.1001.0.0"
-		availableOn = "windows.desktop"
-		maturityRating = "all"
-		cardsEnabled = "true"
-		pzn = "0"
-		pageSize = "25"
-		skipItems = "0"
+		Uri             = "https://storeedgefd.dsx.mp.microsoft.com/v9.0/pages/searchResults?$($Keys)"
+		UseBasicParsing = $true
+		Verbose         = $true
 	}
-
-	[string]$Parameters = (-join ($Parameters.Keys | ForEach-Object -Process {$_ + "=" + $Parameters.$_ + "&"})).TrimEnd("&")
-
-	$URL = @{
-		Name = "URL"
-		Expression = {"https://www.microsoft.com/store/productId/" + $_.ProductId}
-	}
-	((Invoke-WebRequest -UseBasicParsing -Uri ($Base + $Parameters)).Content | ConvertFrom-Json -AsHashtable).Payload.Cards | Select-Object -Property Title, Price, ProductId, $URL
+	Invoke-RestMethod @Parameters
 }
+Search-MSStore -Query "terminal"
