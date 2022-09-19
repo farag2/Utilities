a9d242b268a28b8a2df140245696d786b3314fa4
Dmitry Nefedov <nefedovds@ya.ru>
2022-09-19 22:22:21 +0300

    Update Install_Wireguard.sh

--- WireGuard/Install_Wireguard.sh
+++ WireGuard/Install_Wireguard.sh
@@ -1,17 +1,10 @@
+# https://github.com/complexorganizations/wireguard-manager
+
 apt update && apt upgrade -y
 # Install curl
 apt install curl -y
 
-# reboot
-
 # Change pasword
 passwd
-
-# https://github.com/complexorganizations/wireguard-manager
-# 9 to update script
-# https://pc-01.tech/wireguard-vpn/
-curl https://raw.githubusercontent.com/complexorganizations/wireguard-manager/main/wireguard-manager.sh --create-dirs -o /usr/local/bin/wireguard-manager.sh
-chmod +x /usr/local/bin/wireguard-manager.sh
-bash /usr/local/bin/wireguard-manager.sh
 
 # /etc/wireguard/clients
