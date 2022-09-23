41ebf5f04d7040455ab4c1c53bfc70bfad4d676c
Dmitry Nefedov <nefedovds@ya.ru>
2022-09-23 19:56:46 +0300

    Update Discord.ps1

--- Download/Discord.ps1
+++ Download/Discord.ps1
@@ -1,3 +1,5 @@
+Get-Process -Name Discord -ErrorAction Ignore | Stop-Process -Force
+
 # https://github.com/BetterDiscord/BetterDiscord
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
 $Parameters = @{
@@ -9,53 +11,7 @@ $Parameters = @{
 Invoke-Webrequest @Parameters
 
 Start-Process -FilePath "$DownloadsFolder\BetterDiscord-Windows.exe" -Wait
-
-# https://github.com/mwittrien/BetterDiscordAddons/blob/master/Library/0BDFDB.plugin.js
-# Needed for ReadAllNotificationsButton
-$Parameters = @{
-	Uri             = "https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Library/0BDFDB.plugin.js"
-	OutFile         = "$env:APPDATA\BetterDiscord\plugins\0BDFDB.plugin.js"
-	UseBasicParsing = $true
-	Verbose         = $true
-}
-Invoke-Webrequest @Parameters
-
-# https://github.com/rauenzi/BDPluginLibrary/blob/master/release/0PluginLibrary.plugin.js
-# Needed for HideDisabledEmojis
-$Parameters = @{
-	Uri             = "https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"
-	OutFile         = "$env:APPDATA\BetterDiscord\plugins\0PluginLibrary.plugin.js"
-	UseBasicParsing = $true
-	Verbose         = $true
-}
-Invoke-Webrequest @Parameters
-
-# https://github.com/mwittrien/BetterDiscordAddons/blob/master/Plugins/ReadAllNotificationsButton/ReadAllNotificationsButton.plugin.js
-$Parameters = @{
-	Uri             = "https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Plugins/ReadAllNotificationsButton/ReadAllNotificationsButton.plugin.js"
-	OutFile         = "$env:APPDATA\BetterDiscord\plugins\ReadAllNotificationsButton.plugin.js"
-	UseBasicParsing = $true
-	Verbose         = $true
-}
-Invoke-Webrequest @Parameters
-
-#https://github.com/rauenzi/BetterDiscordAddons/blob/master/Plugins/DoNotTrack/DoNotTrack.plugin.js
-$Parameters = @{
-	Uri             = "https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/DoNotTrack/DoNotTrack.plugin.js"
-	OutFile         = "$env:APPDATA\BetterDiscord\plugins\DoNotTrack.plugin.jss"
-	UseBasicParsing = $true
-	Verbose         = $true
-}
-Invoke-Webrequest @Parameters
-
-# https://github.com/rauenzi/BetterDiscordAddons/blob/master/Plugins/HideDisabledEmojis/HideDisabledEmojis.plugin.js
-$Parameters = @{
-	Uri             = "https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/HideDisabledEmojis/HideDisabledEmojis.plugin.js"
-	OutFile         = "$env:APPDATA\BetterDiscord\plugins\HideDisabledEmojis.plugin.js"
-	UseBasicParsing = $true
-	Verbose         = $true
-}
-Invoke-Webrequest @Parameters
+Remove-Item -Path "$DownloadsFolder\BetterDiscord-Windows.exe" -Force
 
 # https://github.com/TakosThings/Fluent-Discord
 $Parameters = @{
@@ -63,20 +19,50 @@ $Parameters = @{
 	UseBasicParsing = $true
 	Verbose         = $true
 }
-$Tag = (Invoke-RestMethod @Parameters).tag_name
-$Parameters = @{
-	Uri             = "https://github.com/TakosThings/Fluent-Discord/releases/download/$Tag/Fluent-Discord.theme.css"
-	OutFile         = "$env:APPDATA\BetterDiscord\themes\Fluent-Discord.theme.css"
-	UseBasicParsing = $true
-	Verbose         = $true
-}
-Invoke-Webrequest @Parameters
+$FluentDiscordTag = (Invoke-RestMethod @Parameters).tag_name
 
-# https://github.com/oSumAtrIX/BetterDiscordPlugins/blob/master/NitroEmoteAndScreenShareBypass.plugin.js
-$Parameters = @{
-	Uri             = "https://raw.githubusercontent.com/oSumAtrIX/BetterDiscordPlugins/master/NitroEmoteAndScreenShareBypass.plugin.js"
-	OutFile         = "$env:APPDATA\BetterDiscord\plugins\NitroEmoteAndScreenShareBypass.plugin.js"
-	UseBasicParsing = $true
-	Verbose         = $true
+$Plugins = @(
+	# https://github.com/mwittrien/BetterDiscordAddons/blob/master/Library/0BDFDB.plugin.js
+	# Needed for ReadAllNotificationsButton
+	"https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Library/0BDFDB.plugin.js",
+
+	# https://github.com/rauenzi/BDPluginLibrary/blob/master/release/0PluginLibrary.plugin.js
+	# Needed for HideDisabledEmojis
+	"https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js",
+
+	# https://github.com/mwittrien/BetterDiscordAddons/blob/master/Plugins/ReadAllNotificationsButton/ReadAllNotificationsButton.plugin.js
+	"https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/master/Plugins/ReadAllNotificationsButton/ReadAllNotificationsButton.plugin.js",
+
+	# https://github.com/rauenzi/BetterDiscordAddons/blob/master/Plugins/DoNotTrack/DoNotTrack.plugin.js
+	"https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/DoNotTrack/DoNotTrack.plugin.js",
+
+	# https://github.com/rauenzi/BetterDiscordAddons/blob/master/Plugins/HideDisabledEmojis/HideDisabledEmojis.plugin.js
+	"https://raw.githubusercontent.com/rauenzi/BetterDiscordAddons/master/Plugins/HideDisabledEmojis/HideDisabledEmojis.plugin.js",
+
+	# https://github.com/TakosThings/Fluent-Discord
+	"https://github.com/TakosThings/Fluent-Discord/releases/download/$FluentDiscordTag/Fluent-Discord.theme.css",
+
+	# https://github.com/oSumAtrIX/BetterDiscordPlugins/blob/master/NitroEmoteAndScreenShareBypass.plugin.js
+	"https://raw.githubusercontent.com/oSumAtrIX/BetterDiscordPlugins/master/NitroEmoteAndScreenShareBypass.plugin.js"
+)
+foreach ($Plugin in $Plugins)
+{
+	if ($(Split-Path -Path $Plugin -Leaf) -eq "Fluent-Discord.theme.css")
+	{
+		$Parameters = @{
+			Uri             = $Plugin
+			OutFile         = "$env:APPDATA\BetterDiscord\themes\$(Split-Path -Path $Plugin -Leaf)"
+			UseBasicParsing = $true
+			Verbose         = $true
+		}
+		Invoke-Webrequest @Parameters
+	}
+
+	$Parameters = @{
+		Uri             = $Plugin
+		OutFile         = "$env:APPDATA\BetterDiscord\plugins\$(Split-Path -Path $Plugin -Leaf)"
+		UseBasicParsing = $true
+		Verbose         = $true
+	}
+	Invoke-Webrequest @Parameters
 }
-Invoke-Webrequest @Parameters
