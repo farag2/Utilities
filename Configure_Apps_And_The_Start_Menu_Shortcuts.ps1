041af95a2154e101ad17520d4ce307a371368bdc
Dmitry Nefedov <nefedovds@ya.ru>
2022-09-23 15:21:22 +0300

    Update Configure_Apps_And_The_Start_Menu_Shortcuts.ps1

--- Configure_Apps_And_The_Start_Menu_Shortcuts.ps1
+++ Configure_Apps_And_The_Start_Menu_Shortcuts.ps1
@@ -234,7 +234,12 @@ if (Test-Path -Path "$env:ProgramFiles\Notepad++")
 	Remove-Item -Path $Remove -Recurse -Force -ErrorAction Ignore
 	Remove-Item -Path "$env:ProgramFiles\Notepad++\localization" -Exclude russian.xml -Recurse -Force -ErrorAction Ignore
 
-	New-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\CLSID\{B298D29A-A6ED-11DE-BA8C-A68E55D89593}\Settings" -Name Title -PropertyType String -Value "Открыть с помощью &Notepad++" -Force
+	if ((Get-WinSystemLocale).Name -eq "ru-RU")
+	{
+		[System.Console]::OutputEncoding = [System.Console]::InputEncoding = [System.Text.Encoding]::UTF8
+
+		New-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\CLSID\{B298D29A-A6ED-11DE-BA8C-A68E55D89593}\Settings" -Name Title -PropertyType String -Value "Открыть с помощью &Notepad++" -Force
+	}
 	New-ItemProperty -Path "HKCU:\SOFTWARE\Classes\Local Settings\Software\Microsoft\Windows\Shell\MuiCache" -Name "C:\Program Files\Notepad++\notepad++.exe.FriendlyAppName" -PropertyType String -Value "Notepad++" -Force
 
 	cmd.exe --% /c ftype txtfile=%ProgramFiles%\Notepad++\notepad++.exe "%1"
