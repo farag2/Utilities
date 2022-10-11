54372cb6f6d125435bb7eded040d2abd24e0e470
Dmitry Nefedov <nefedovds@ya.ru>
2022-10-11 20:55:57 +0300

    Update yt-dlp.ps1

--- yt-dlp.ps1
+++ yt-dlp.ps1
@@ -3,7 +3,7 @@
 	Download videos from YoutTube via yt-dlp
 
 	.EXAMPLE
-	youtube-dl -Path "D:\yt-dlp.exe" -URLs @() -Format "22+251"
+	youtube-dl -URLs @()
 
 	.NOTES
 	Invoke "D:\yt-dlp.exe" --list-formats URL
@@ -17,19 +17,14 @@
 	https://github.com/yt-dlp/yt-dlp
 	https://github.com/BtbN/FFmpeg-Builds
 #>
+cls
 function yt-dlp
 {
 	[CmdletBinding()]
 	param
 	(
-		[string]
-		$Path,
-
 		[string[]]
-		$URLs,
-
-		[string]
-		$Format
+		$URLs
 	)
 
 	# Get the latest youtube-dl build tag
@@ -40,14 +35,20 @@ function yt-dlp
 	$LatestytdlplRelease = (Invoke-RestMethod @Parameters).tag_name
 
 	$DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
-
-	$Parameters = @{
-		Uri              = "https://github.com/yt-dlp/yt-dlp/releases/download/$LatestytdlplRelease/yt-dlp.exe"
-		OutFile          = "$DownloadsFolder\yt-dlp.exe"
-		UseBasicParsing  = $true
-		Verbose          = $true
-	}
-	Invoke-WebRequest @Parameters
+    if (Test-Path -Path "$DownloadsFolder\yt-dlp.exe")
+    {
+        $CurrentVersion = (Get-Item -Path "$DownloadsFolder\yt-dlp.exe").VersionInfo.FileVersion
+        if ([System.Version]$LatestytdlplRelease -gt [System.Version]$CurrentVersion)
+        {
+	        $Parameters = @{
+		        Uri              = "https://github.com/yt-dlp/yt-dlp/releases/download/$LatestytdlplRelease/yt-dlp.exe"
+		        OutFile          = "$DownloadsFolder\yt-dlp.exe"
+		        UseBasicParsing  = $true
+		        Verbose          = $true
+	        }
+	        Invoke-WebRequest @Parameters
+        }
+    }
 
 	# Get the latest FFmpeg URL
 	# "ffmpeg-*-win64-lgpl-[0-9].[0-9].zip"
@@ -57,39 +58,42 @@ function yt-dlp
 		UseBasicParsing  = $true
 	}
 	$LatestFFmpegURL = ((Invoke-RestMethod @Parameters).assets | Where-Object -FilterScript {$_.name -match "ffmpeg-n5.1-latest-win64-gpl-5.1.zip"}).browser_download_url
-	$Parameters = @{
-		Uri              = $LatestFFmpegURL
-		OutFile          = "$DownloadsFolder\FFmpeg.zip"
-		UseBasicParsing  = $true
-		Verbose          = $true
-	}
-	Invoke-WebRequest @Parameters
 
-	# Expand ffmpeg.exe from the ZIP archive
-	Add-Type -Assembly System.IO.Compression.FileSystem
+    if (-not (Test-Path -Path "$DownloadsFolder\ffmpeg.exe"))
+    {
+	    $Parameters = @{
+		    Uri              = $LatestFFmpegURL
+		    OutFile          = "$DownloadsFolder\FFmpeg.zip"
+		    UseBasicParsing  = $true
+		    Verbose          = $true
+	    }
+	    Invoke-WebRequest @Parameters
 
-	$ZIP = [IO.Compression.ZipFile]::OpenRead("$DownloadsFolder\FFmpeg.zip")
-	$Entries = $ZIP.Entries | Where-Object -FilterScript {$_.FullName -like "ffmpeg*/bin/ffmpeg.exe"}
-	$Entries | ForEach-Object -Process {[IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$DownloadsFolder\ffmpeg.exe", $true)}
-	$ZIP.Dispose()
+	    # Expand ffmpeg.exe from the ZIP archive
+	    Add-Type -Assembly System.IO.Compression.FileSystem
 
-	Remove-Item -Path "$DownloadsFolder\FFmpeg.zip" -Force
+	    $ZIP = [IO.Compression.ZipFile]::OpenRead("$DownloadsFolder\FFmpeg.zip")
+	    $Entries = $ZIP.Entries | Where-Object -FilterScript {$_.FullName -like "ffmpeg*/bin/ffmpeg.exe"}
+	    $Entries | ForEach-Object -Process {[IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$DownloadsFolder\ffmpeg.exe", $true)}
+	    $ZIP.Dispose()
 
-	$OutputFolder = Split-Path -Path $Path -Parent
+	    Remove-Item -Path "$DownloadsFolder\FFmpeg.zip" -Force
+    }
+
 	$Title = "%(title)s.mp4"
-
 	$n = 1
+
 	foreach ($URL in $URLs)
 	{
+        # Getting URL's IDs
+        & "$DownloadsFolder\yt-dlp.exe" --list-formats $URLs
+        $AudioID = Read-Host -Prompt "`nType prefered audio ID"
+        $VideoID = Read-Host -Prompt "`nType prefered video ID"
+
 		# 1. FileName.mp4
 		$FileName = "{0}. {1}" -f $n++, $Title
 
-		Start-Process -FilePath $Path -ArgumentList "--output `"$OutputFolder\$FileName`" --format $Format $URL"
+		Start-Process -FilePath "$DownloadsFolder\yt-dlp.exe" -ArgumentList "--output `"$DownloadsFolder\$FileName`" --format `"$($AudioID)+$($VideoID)`" $URL"
 	}
 }
-# Run first to get URL's IDs to use
-& "D:\Downloads\yt-dlp.exe" --list-formats "https://www.youtube.com/watch?v=DqTEVmed0Bc"
-
-# Uncomment and run next when you got on the previous step URL's IDs
-# yt-dlp -Path "D:\Downloads\yt-dlp.exe" -URLs @("https://www.youtube.com/watch?v=DqTEVmed0Bc") -Format "248+140"
-
+yt-dlp -URLs @("https://www.youtube.com/watch?v=DqTEVmed0Bc")
