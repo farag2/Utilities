5a15ee977195a1fa49c1fc98b784e23841c13d89
Dmitry Nefedov <nefedovds@ya.ru>
2022-11-13 19:26:53 +0300

    Delete Activation.ps1

--- diag/Activation.ps1
+++
@@ -1,4 +0,0 @@
-﻿New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Adobe\Adobe Acrobat\DC\Activation" -Name IsAMTEnforced -PropertyType DWord -Value 1 -Force
-New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Adobe\Adobe Acrobat\DC\Activation" -Name DisabledActivation -PropertyType DWord -Value 1 -Force
-New-ItemProperty -Path "HKLM:\SOFTWARE\Adobe\Adobe Acrobat\DC\Activation" -Name IsAMTEnforced -PropertyType DWord -Value 1 -Force
-New-ItemProperty -Path "HKLM:\SOFTWARE\Adobe\Adobe Acrobat\DC\Activation" -Name DisabledActivation -PropertyType DWord -Value 1 -Force
