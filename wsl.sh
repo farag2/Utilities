0671c4c7f81714cc284114eba31855fc3b140dd4
Dmitry Nefedov <nefedovds@ya.ru>
2023-02-05 13:58:22 +0300

    Update and rename wsl.txt to wsl.sh

--- wsl.sh
+++ wsl.sh
@@ -2,10 +2,13 @@
 wslconfig /list /all
 
 # Where all distros installed
-%USERPROFILE%\AppData\Local\Packages
+%USERPROFILE%\AppData\Local\Packages\<package name>
 
 # Copy files from Windows to WSL (/home/<username>). d is a drive letter
 cp /mnt/d/temp.txt $HOME
 
 # Connect to distros via the File Explorer
 \\wsl$
+
+# Print all env variables
+printenv
