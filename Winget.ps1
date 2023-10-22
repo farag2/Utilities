52ff4a2f886140d9e97fa379a6fc02aac7c4bd62
Dmitry Nefedov <nefedovds@ya.ru>
2023-10-22 14:53:39 +0300

    Delete Winget.ps1

--- Winget.ps1
+++
@@ -1,8 +0,0 @@
-# https://winstall.app
-# https://docs.microsoft.com/en-us/windows/package-manager/winget
-
-# Find Firefox
-winget search firefox --accept-source-agreements
-
-# Install
-winget install --id Mozilla.Firefox
