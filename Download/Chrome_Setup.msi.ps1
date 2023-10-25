cad5b518646a05b224202e40667aeca98952d317
Dmitry Nefedov <nefedovds@ya.ru>
2023-10-25 23:54:26 +0300

    Update Chrome_Setup.msi.ps1

--- Download/Chrome_Setup.msi.ps1
+++ Download/Chrome_Setup.msi.ps1
@@ -1,6 +1,13 @@
 # Downloading the latest Chrome
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+if ($Host.Version.Major -eq 5)
+{
+	# Progress bar can significantly impact cmdlet performance
+	# https://github.com/PowerShell/PowerShell/issues/2138
+	$Script:ProgressPreference = "SilentlyContinue"
+}
+
 # https://chromeenterprise.google/browser/download
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
 $Parameters = @{
