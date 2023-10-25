cfb35f3ce908f771129feb7952cda766c751e1d3
Dmitry Nefedov <nefedovds@ya.ru>
2023-10-25 23:54:36 +0300

    Update Edge.ps1

--- Download/Edge.ps1
+++ Download/Edge.ps1
@@ -1,6 +1,13 @@
 # Download Microsoft Edge Stable x64
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+if ($Host.Version.Major -eq 5)
+{
+	# Progress bar can significantly impact cmdlet performance
+	# https://github.com/PowerShell/PowerShell/issues/2138
+	$Script:ProgressPreference = "SilentlyContinue"
+}
+
 $Parameters = @{
 	Uri             = "https://edgeupdates.microsoft.com/api/products"
 	UseBasicParsing = $true
