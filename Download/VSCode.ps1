acd5941168314e665afb5b4c80ebcaeafafb92b0
Dmitry Nefedov <nefedovds@ya.ru>
2023-10-25 23:55:20 +0300

    Update VSCode.ps1

--- Download/VSCode.ps1
+++ Download/VSCode.ps1
@@ -1,3 +1,12 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+if ($Host.Version.Major -eq 5)
+{
+	# Progress bar can significantly impact cmdlet performance
+	# https://github.com/PowerShell/PowerShell/issues/2138
+	$Script:ProgressPreference = "SilentlyContinue"
+}
+
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
 $Parameters = @{
 	Uri     = "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64"
