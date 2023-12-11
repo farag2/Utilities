9758f0005d3d794b0afb4925beea76dfa40ecdec
Dmitry Nefedov <nefedovds@ya.ru>
2023-12-11 13:33:33 +0300

    Update GZDoom.ps1

--- Download/GZDoom/GZDoom.ps1
+++ Download/GZDoom/GZDoom.ps1
@@ -18,9 +18,10 @@ $URL = ((Invoke-RestMethod @Parameters).assets | Where-Object -FilterScript {$_.
 
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
 $Parameters = @{
-	Uri     = $URL
-	OutFile = "$DownloadsFolder\GZDoom.zip"
-	Verbose = $true
+	Uri             = $URL
+	OutFile         = "$DownloadsFolder\GZDoom.zip"
+	UseBasicParsing = $true
+	Verbose         = $true
 }
 Invoke-WebRequest @Parameters
 
@@ -32,18 +33,12 @@ $Parameters = @{
 }
 Expand-Archive @Parameters
 
-$Parameters = @(
-	"$DownloadsFolder\GZDoom.zip",
-	"$DownloadsFolder\GZDoom\fm_banks",
-	"$DownloadsFolder\GZDoom\licenses.zip"
-)
-Remove-Item -Path $Parameters -Recurse -Force
-
 # https://github.com/farag2/Utilities/tree/master/Download/GZDoom
 $Parameters = @{
-	Uri     = "https://raw.githubusercontent.com/farag2/Utilities/master/Download/GZDoom/gzdoom.ini"
-	OutFile = "$DownloadsFolder\GZDoom\gzdoom.ini"
-	Verbose = $true
+	Uri             = "https://raw.githubusercontent.com/farag2/Utilities/master/Download/GZDoom/gzdoom.ini"
+	OutFile         = "$DownloadsFolder\GZDoom\gzdoom.ini"
+	UseBasicParsing = $true
+	Verbose         = $true
 }
 Invoke-WebRequest @Parameters
 
@@ -51,29 +46,24 @@ Get-Item -Path "$DownloadsFolder\GZDoom\gzdoom.ini" -Force | Rename-Item -NewNam
 
 # https://github.com/farag2/Utilities/tree/master/Download/GZDoom
 $Parameters = @{
-	Uri     = "https://raw.githubusercontent.com/farag2/Utilities/master/Download/GZDoom/_pb.cmd"
-	OutFile = "$DownloadsFolder\GZDoom\_pb.cmd"
-	Verbose = $true
+	Uri             = "https://raw.githubusercontent.com/farag2/Utilities/master/Download/GZDoom/_pb.cmd"
+	OutFile         = "$DownloadsFolder\GZDoom\_pb.cmd"
+	UseBasicParsing = $true
+	Verbose         = $true
 }
 Invoke-WebRequest @Parameters
 
 # https://github.com/farag2/Utilities/tree/master/Download/GZDoom
 $Parameters = @{
-	Uri     = "https://raw.githubusercontent.com/farag2/Utilities/master/Download/GZDoom/_bd.cmd"
-	OutFile = "$DownloadsFolder\GZDoom\_bd.cmd"
-	Verbose = $true
+	Uri             = "https://raw.githubusercontent.com/farag2/Utilities/master/Download/GZDoom/_bd.cmd"
+	OutFile         = "$DownloadsFolder\GZDoom\_bd.cmd"
+	UseBasicParsing = $true
+	Verbose         = $true
 }
 Invoke-WebRequest @Parameters
 
 # https://www.moddb.com/mods/brutal-doom/downloads/brutal-doom-v21-beta
-# Downloading Brutal Doom v21
-# Skip Internet Explorer first run wizard to let script use Trident function to parse sites
-if (-not (Test-Path -Path "HKLM:\Software\Policies\Microsoft\Internet Explorer\Main"))
-{
-	New-Item -Path "HKLM:\Software\Policies\Microsoft\Internet Explorer\Main" -ItemType Directory -Force
-}
-New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Internet Explorer\Main" -Name DisableFirstRunCustomize -PropertyType String -Value 1 -Force
-
+# Expand archive manualy
 $Parameters = @{
 	Uri             = "https://www.moddb.com/downloads/start/95667"
 	UseBasicParsing = $false # Disabled
@@ -83,17 +73,24 @@ $Request = Invoke-Webrequest @Parameters
 $URL = $Request.ParsedHtml.getElementsByTagName("a") | ForEach-Object -Process {$_.pathname} | Where-Object -FilterScript {$_ -match "mirror"}
 $Parameters = @{
 	Uri             = "https://www.moddb.com/$URL"
-	OutFile         = "$DownloadsFolder\db.rar"
+	OutFile         = "$DownloadsFolder\db.zip"
 	UseBasicParsing = $true
 	Verbose         = $true
 }
 Invoke-Webrequest @Parameters
 
-# 
+# https://github.com/farag2/Utilities/tree/master/Download/GZDoom
 $Parameters = @{
-	Uri             = "http://iddqd.ru/download?idclick=doom/levels/doom2_wad.7z&title=Doom%202:%20Hell%20on%20Earth&where=levels"
-	OutFile         = "$DownloadsFolder\doom2.7z"
+	Uri             = "https://github.com/farag2/Utilities/raw/master/Download/GZDoom/doom2.wad"
+	OutFile         = "$DownloadsFolder\GZDoom\doom2.wad"
 	UseBasicParsing = $true
 	Verbose         = $true
 }
 Invoke-Webrequest @Parameters
+
+$Parameters = @(
+	"$DownloadsFolder\GZDoom.zip",
+	"$DownloadsFolder\GZDoom\fm_banks",
+	"$DownloadsFolder\GZDoom\licenses.zip"
+)
+Remove-Item -Path $Parameters -Recurse -Force
