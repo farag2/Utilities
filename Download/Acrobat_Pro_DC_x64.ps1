2ec2c69c3ff3521983c64e0bf65991876b19ecf4
Dmitry Nefedov <nefedovds@ya.ru>
2024-03-23 23:10:42 +0300

    Update Acrobat_Pro_DC_x64.ps1

--- Download/Acrobat_Pro_DC_x64.ps1
+++ Download/Acrobat_Pro_DC_x64.ps1
@@ -12,6 +12,7 @@ if ($Host.Version.Major -eq 5)
 	$Script:ProgressPreference = "SilentlyContinue"
 }
 
+# https://helpx.adobe.com/enterprise/using/deploying-acrobat.html
 $DownloadsFolder = Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "{374DE290-123F-4565-9164-39C4925E467B}"
 $Parameters = @{
 	Uri             = "https://trials.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/win32/Acrobat_DC_Web_x64_WWMUI.zip"
