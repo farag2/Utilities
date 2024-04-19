024fc0281f5a5829008853e94a52d541c3b40e0e
Dmitry Nefedov <nefedovds@ya.ru>
2024-04-19 16:21:32 +0300

    Update Trigger-Schedule.ps1

--- AD/Trigger-Schedule.ps1
+++ AD/Trigger-Schedule.ps1
@@ -17,7 +17,11 @@
 	.NOTES
 	https://docs.microsoft.com/en-us/mem/configmgr/develop/reference/core/clients/client-classes/triggerschedule-method-in-class-sms_client
 	https://stackoverflow.com/a/63916401/8315671
+	https://www.manishbangia.com/initiate-sccm-client-actions-cmd-line/
 #>
+
+#Requires -RunAsAdministrator
+
 function Trigger-Schedule
 {
 	[CmdletBinding()]
@@ -45,6 +49,7 @@ function Trigger-Schedule
 		$All
 	)
 
+	# https://learn.microsoft.com/en-us/mem/configmgr/develop/reference/core/clients/client-classes/triggerschedule-method-in-class-sms_client#parameters
 	$SupportedSchedules = @{
 		"AppDeployment"                    = "{00000000-0000-0000-0000-000000000121}"
 		"DiscoveryData"                    = "{00000000-0000-0000-0000-000000000003}"
@@ -55,24 +60,24 @@ function Trigger-Schedule
 		"SoftwareMeteringUsage"            = "{00000000-0000-0000-0000-000000000031}"
 		"SoftwareUpdates"                  = "{00000000-0000-0000-0000-000000000108}"
 		"SoftwareUpdatesScan"              = "{00000000-0000-0000-0000-000000000113}"
+		"PolicyAgentRequestAssignment"     = "{00000000-0000-0000-0000-000000000026}"
 		"UserPolicyRetrievalEvalCycle"     = "{00000000-0000-0000-0000-000000000027}"
 		"WindowsInstallerSourceListUpdate" = "{00000000-0000-0000-0000-000000000032}"
 	}
 
+	$SID = (Get-CimInstance -Namespace root\ccm -ClassName CCM_UserLogonEvents | Where-Object -FilterScript {$null -eq $_.LogoffTime}).UserSID.Replace("-", "_")
+
 	foreach ($Schedule in $Schedules)
 	{
-		if ($Schedule -eq "UserPolicyRetrievalEvalCycle") # "Policy Agent Request Assignment" too
-		{
-			Write-Verbose -Message $SupportedSchedules[$Schedule] -Verbose
+		Write-Verbose -Message $SupportedSchedules[$Schedule] -Verbose
 
-			$SID = (Get-CimInstance -Namespace root\ccm -ClassName CCM_UserLogonEvents | Where-Object -FilterScript {$null -eq $_.LogoffTime}).UserSID.Replace("-", "_")
+		if (($Schedule -eq "PolicyAgentRequestAssignment") -or ($Schedule -eq "UserPolicyRetrieval"))
+		{
 			$ActualConfig = Get-WmiObject -ClassName CCM_Scheduler_ScheduledMessage -Namespace root\ccm\Policy\$SID\ActualConfig | Where-Object -FilterScript {$_.ScheduledMessageID -eq $SupportedSchedules[$Schedule]}
 			$ActualConfig.Triggers = @('SimpleInterval;Minutes=1;MaxRandomDelayMinutes=0')
 			$ActualConfig.Put()
 
-			Start-Sleep -Seconds 10
-
-			# $SID = (Get-CimInstance -namespace root\ccm -ClassName CCM_UserLogonEvents | Where-Object -FilterScript {$null -eq $_.LogoffTime}).UserSID.Replace("-", "_")
+			# $SID = (Get-CimInstance -Namespace root\ccm -ClassName CCM_UserLogonEvents | Where-Object -FilterScript {$null -eq $_.LogoffTime}).UserSID.Replace("-", "_")
 			# $ActualConfig = Get-CimInstance -Namespace root\ccm\Policy\$SID\ActualConfig -ClassName CCM_Scheduler_ScheduledMessage | Where-Object -FilterScript {$_.ScheduledMessageID -eq $Schedule}
 			# $ActualConfig.Triggers = @('SimpleInterval;Minutes=1;MaxRandomDelayMinutes=0')
 			# $ActualConfig | Set-CimInstance
@@ -90,14 +95,11 @@ function Trigger-Schedule
 		{
 			Write-Verbose -Message $Schedule -Verbose
 
-			if ($Schedule -eq "{00000000-0000-0000-0000-000000000027}") # "Policy Agent Request Assignment" too
+			if (($Schedule -eq "PolicyAgentRequestAssignment") -or ($Schedule -eq "UserPolicyRetrieval"))
 			{
-				$SID = (Get-CimInstance -Namespace root\ccm -ClassName CCM_UserLogonEvents | Where-Object -FilterScript {$null -eq $_.LogoffTime}).UserSID.Replace("-", "_")
 				$ActualConfig = Get-WmiObject -ClassName CCM_Scheduler_ScheduledMessage -Namespace root\ccm\Policy\$SID\ActualConfig | Where-Object -FilterScript {$_.ScheduledMessageID -eq $Schedule}
 				$ActualConfig.Triggers = @('SimpleInterval;Minutes=1;MaxRandomDelayMinutes=0')
 				$ActualConfig.Put()
-
-				Start-Sleep -Seconds 10
 
 				# $SID = (Get-CimInstance -Namespace root\ccm -ClassName CCM_UserLogonEvents | Where-Object -FilterScript {$null -eq $_.LogoffTime}).UserSID.Replace("-", "_")
 				# $ActualConfig = Get-CimInstance -ClassName CCM_Scheduler_ScheduledMessage -Namespace root\ccm\Policy\$SID\ActualConfig | Where-Object -FilterScript {$_.ScheduledMessageID -eq $Schedule}
@@ -107,9 +109,41 @@ function Trigger-Schedule
 			else
 			{
 				Invoke-CimMethod -Namespace root\ccm -ClassName SMS_Client -MethodName TriggerSchedule -Arguments @{sScheduleID = $Schedule}
-
-				Start-Sleep -Seconds 10
 			}
 		}
+
+		<#
+		$SupportedSchedules = (Get-CimInstance -ClassName CCM_Scheduler_ScheduledMessage -namespace "ROOT\ccm\policy\machine\actualconfig" | Where-Object -FilterScript {$_.ScheduledMessageID -like "{00000000-0000-0000-0000-*}"}).ScheduledMessageID
+		foreach ($SupportedSchedule in $SupportedSchedules)
+		{
+			try
+			{
+				Invoke-CimMethod -Namespace root\ccm -ClassName SMS_Client -MethodName TriggerSchedule -Arguments @{sScheduleID = $SupportedSchedule} -ErrorAction Stop
+				"$SupportedSchedule success"
+			}
+			catch [Microsoft.Management.Infrastructure.CimException]
+			{
+				"$SupportedSchedule failed"
+			}
+		}
+		#>
 	}
 }
+<#
+$Schedules = @(
+	"AppDeployment",
+	"DiscoveryData",
+	"FileCollection",
+	"HardwareInventory",
+	"MachinePolicyRetrieval",
+	"SoftwareInventory",
+	"SoftwareMeteringUsage",
+	"SoftwareUpdates",
+	"SoftwareUpdatesScan",
+	"PolicyAgentRequestAssignment",
+	"UserPolicyRetrievalEvalCycle",
+	"WindowsInstallerSourceListUpdate"
+)
+Trigger-Schedule -Schedules $Schedules
+#>
+Trigger-Schedule -All
