daabf98429cd9721cdaa0c76fe0a13363865a63c
Dmitry Nefedov <nefedovds@ya.ru>
2024-09-29 21:02:37 +0300

    Update Configure.sh

--- VPN/Configure.sh
+++ VPN/Configure.sh
@@ -18,7 +18,32 @@ passwd
 # /var/logs
 
 # Disable ICMP ping response
-/etc/sysctl.conf: net.ipv4.icmp_echo_ignore_all = 1
+# /etc/sysctl.conf
+net.ipv4.icmp_echo_ignore_all = 1
+# Enable BBR
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
+#
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.netdev_max_backlog = 10000
+net.core.somaxconn = 4096
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_keepalive_time = 1200 
+net.ipv4.tcp_keepalive_probes = 5
+net.ipv4.tcp_keepalive_intvl = 30
+net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_mem = 25600 51200 102400
+net.ipv4.udp_mem = 25600 51200 102400
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_mtu_probing = 1 
+net.ipv4.tcp_slow_start_after_idle=0
+#
 sysctl -p
 
 https://github.com/XTLS/Xray-core
