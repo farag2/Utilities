737b037dec413441a26c9d6d3dc7637494355e0b
Dmitry Nefedov <nefedovds@ya.ru>
2025-04-26 23:27:42 +0300

    Create nginx.sh

---
+++ Linux/nginx.sh
@@ -0,0 +1,23 @@
+# Enable permanent redirection
+sudo nano /etc/nginx/sites-available/<subdomain>
+
+# /etc/nginx/sites-available/<subdomain> file
+server {
+    listen 80;
+    server_name <subdomain>;
+
+    # 301 Permanent Redirect
+    return 301 <new_domain>;
+}
+
+sudo ln -s /etc/nginx/sites-available/<subdomain> /etc/nginx/sites-enabled/
+sudo nginx -t && sudo systemctl reload nginx
+
+# In ISPmanager
+Name: subdomain_name
+Type: A
+Value: <VPS IP>
+TTL: 3600
+
+# Test configuration
+curl -I <subdomain>
