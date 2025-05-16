5d463af005aec013a1119032ddfcdf456b20b0bd
Dmitry Nefedov <nefedovds@ya.ru>
2025-05-16 16:41:30 +0300

    Update Download_Install_Updates_for_Drivers.ps1

--- Download_Install_Updates_for_Drivers.ps1
+++ Download_Install_Updates_for_Drivers.ps1
@@ -1,35 +1,42 @@
-$Session            = New-Object -ComObject Microsoft.Update.Session
-$Searcher           = $Session.CreateUpdateSearcher()
+#Requires -RunAsAdministrator
+#Requires -Version 5.1
 
+$Updates = @()
+
+$Session = New-Object -ComObject Microsoft.Update.Session
+$Searcher = $Session.CreateUpdateSearcher()
 $Searcher.ServiceID = "7971f918-a847-4430-9279-4a52d1efe18d"
-# MachineOnly
-$Searcher.SearchScope     =  1
-# Third Party
-$Searcher.ServerSelection = 3
 
 Write-Verbose -Message "Searching Driver Updates..." -Verbose
 
 $SearchResult = $Searcher.Search("IsInstalled=0 and Type='Driver' and IsHidden=0")
-$Updates      = $SearchResult.Updates
+$Updates = $SearchResult.Updates
 
-#Show available Drivers
-$Updates | Select-Object -Property Title, DriverModel, DriverVerDate, Driverclass, DriverManufacturer
-
-#Download the Drivers from Microsoft
-$UpdatesToDownload = New-Object -Com Microsoft.Update.UpdateColl
-$Updates | ForEach-Object -Process {
-	$UpdatesToDownload.Add($_) | Out-Null
+if ($Updates.Count -gt 0)
+{
+	$Updates | Select-Object -Property Title, DriverModel, DriverVerDate, Driverclass, DriverManufacturer
 }
+else
+{
+	Write-Verbose -Message "No updates available" -Verbose
+	exit
+}
+
+	$Updates | ForEach-Object -Process {$_.AcceptEula()}
 
 Write-Verbose -Message "Downloading Drivers..." -Verbose
 
-$UpdateSession      = New-Object -Com Microsoft.Update.Session
-$Downloader         = $UpdateSession.CreateUpdateDownloader()
-$Downloader.Updates = $UpdatesToDownload
+$UpdateToDownload = New-Object -ComObject Microsoft.Update.UpdateColl
+$Updates | ForEach-Object -Process {
+	$UpdateToDownload.Add($_) | Out-Null
+}
+
+$Downloader = $Session.CreateUpdateDownloader()
+$Downloader.Updates = $UpdateToDownload
 $Downloader.Download()
 
-# Check if the Drivers are all downloaded and trigger the Installation
-$UpdatesToInstall = New-Object -Com Microsoft.Update.UpdateColl
+# Check if drivers were downloaded and trigger installation
+$UpdatesToInstall = New-Object -ComObject Microsoft.Update.UpdateColl
 $Updates | ForEach-Object -Process {
 	if ($_.IsDownloaded)
 	{
@@ -39,8 +46,8 @@ $Updates | ForEach-Object -Process {
 
 Write-Verbose -Message "Installing Drivers..." -Verbose
 
-$Installer          = $UpdateSession.CreateUpdateInstaller()
-$Installer.Updates  = $UpdatesToInstall
+$Installer = $Session.CreateUpdateInstaller()
+$Installer.Updates = $UpdatesToInstall
 $InstallationResult = $Installer.Install()
 
 if ($InstallationResult.RebootRequired)
