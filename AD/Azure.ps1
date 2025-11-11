7bbbfe8a3c812c16553b17fac04b2041d32ed54a
Dmitry Nefedov <nefedovds@ya.ru>
2025-11-11 14:12:58 +0300

    Update Azure.ps1

--- AD/Azure.ps1
+++ AD/Azure.ps1
@@ -1,63 +1,184 @@
-# https://www.powershellgallery.com/packages/AzureAD/
-Connect-AzureAD
+# https://www.powershellgallery.com/packages/Microsoft.Graph
+Install-Module -Name Microsoft.Graph -Force
+Connect-MgGraph -Scopes "User.Read.All"
 
-# Create table which laptop is assigned to user
-$DeviceIDs = @(
-	"hostname1"
-)
-foreach ($DeviceID in $DeviceIDs)
+# Get iPad properties
+$UserIDs = Get-ADUser -SearchBase "OU=RU,OU=Corp,DC=fr,DC=COMPANY,DC=lcl" -Filter * -Properties * | Where-Object {($_.employeeType -ne "SERVICE") -and $_.Enabled}
+foreach ($UserID in $UserIDs)
 {
-	Get-AzureADDevice -All $true -Searchstring $DeviceID | ForEach-Object -Process {
-		Get-AzureADDeviceRegisteredOwner -ObjectId $_.ObjectId | ForEach-Object -Process {
-			[PSCustomObject]@{
-				DeviceID          = $DeviceID
-				DisplayName       = $_.DisplayName
-				UserPrincipalName = $_.UserPrincipalName
-				MailNickName      = $_.MailNickName
-				Managed           = $_.IsManaged
-			} | Format-Table
+	if (Get-MgUserOwnedDevice -UserId $UserID.UserPrincipalName -ErrorAction Ignore)
+	{
+		Get-MgUserOwnedDevice -UserId $UserID.UserPrincipalName -ErrorAction Ignore | ForEach-Object -Process {
+			Get-MgDevice -DeviceId $_.Id -Property * | Where-Object -FilterScript {
+				($_.Manufacturer -eq "Apple") -and ([System.Version]$_.OperatingSystemVersion -gt [System.Version]"18.3") -and ([System.Version]$_.OperatingSystemVersion -lt [System.Version]"26.1") -and ($_.OperatingSystem -eq "iOS")
+			} | ForEach-Object -Process {
+				[PSCustomObject]@{
+					Surname  = $UserID.Surname
+					hostname = $_.DisplayName
+					Model    = $_.Model
+					OS       = $_.OperatingSystem
+					Build    = $_.OperatingSystemVersion
+					"s/n"    = $_.SerialNumber
+				}
+			} | Format-Table -AutoSize
 		}
 	}
 }
 
-# https://www.powershellgallery.com/packages/MSOnline
-Connect-MsolService
-
-# Create table with users who has laptop assigned
-$Users = @(Get-ADUser -SearchBase "OU=RU, OU=EMEA, OU=USR, OU=CORP, DC=eur, DC=mccormick, DC=com" -Filter * -Properties *)
-foreach ($User in $Users)
+# Get Windows based laptops properties
+# https://www.powershellgallery.com/packages/Microsoft.Graph.DeviceManagement
+# https://www.powershellgallery.com/packages/Microsoft.Graph
+$UserIDs = Get-ADUser -SearchBase "OU=RU,OU=Corp,DC=fr,DC=COMPANY,DC=lcl" -Filter * -Properties * | Where-Object {($_.employeeType -ne "SERVICE") -and $_.Enabled}
+foreach ($UserID in $UserIDs)
 {
-	$hostname = if ($null -ne (Get-MsolDevice -RegisteredOwnerUpn $User.UserPrincipalName -ErrorAction Ignore))
+	if (Get-MgUserOwnedDevice -UserId $UserID.UserPrincipalName -ErrorAction Ignore)
 	{
-		[string](Get-MsolDevice -RegisteredOwnerUpn $User.UserPrincipalName | Where-Object -FilterScript {
-			($_.DeviceOsType -eq "Windows") -and ($_.DeviceTrustType -eq "Domain Joined")}
-		).DisplayName
+		Get-MgUserOwnedDevice -UserId $UserID.UserPrincipalName -ErrorAction Ignore | ForEach-Object -Process {
+			Get-MgDevice -DeviceId $_.Id -Property * | Where-Object {$_.OperatingSystem -eq "Windows"} | ForEach-Object {
+				[PSCustomObject]@{
+					Surname          = $UserID.Surname
+					Manufacturer     = $_.Manufacturer
+					Model            = $_.Model
+					"s/n"            = $_.SerialNumber
+					hostname         = $_.DisplayName
+					Build            = $_.OperatingSystemVersion
+					"Last Sync Time" = $_.OnPremisesLastSyncDateTime
+				} | Format-Table -AutoSize
+			}
+		}
 	}
-
-	[PSCustomObject]@{
-		userID            = $User.SamAccountName
-		email             = $User.UserPrincipalName
-		hostname          = "$hostname"
-		"Last logon date" = $User.LastLogonDate
-	} | Select-Object -Property userID, email, hostname, "Last logon date" | Export-Csv -Path "D:\list.csv" -NoTypeInformation -Delimiter ';' -Append -Force
 }
 
-
-
-# https://www.powershellgallery.com/packages/Microsoft.Graph.Intune
-Connect-MSGraph
-
-# Find users in OU which have laptops assigned and create table with laptops models and s/n
-$Users = @(Get-ADUser -SearchBase "OU=, OU=, OU=, OU=, DC=, DC=, DC=com" -Filter * -Properties *)
-foreach ($User in $Users)
+# Export laptop properties
+# https://www.powershellgallery.com/packages/Microsoft.Graph.DeviceManagement
+# https://www.powershellgallery.com/packages/Microsoft.Graph
+$UserIDs = Get-ADUser -SearchBase "OU=RU,OU=Corp,DC=fr,DC=COMPANY,DC=lcl" -Filter * -Properties * | Where-Object {($_.employeeType -ne "SERVICE") -and $_.Enabled}
+foreach ($UserID in $UserIDs)
 {
-	Get-IntuneManagedDevice -Filter "contains(operatingsystem, 'Windows')" | Get-MSGraphAllPages | Where-Object -FilterScript {$_.emailAddress -eq $User.UserPrincipalName} | ForEach-Object -Process {
+	$Devices = Get-MgDeviceManagementManagedDevice -All -Filter "OperatingSystem eq 'Windows' and UserPrincipalName eq '$($UserID.UserPrincipalName)'" -Property *
+	foreach ($Device in $Devices)
+	{
 		[PSCustomObject]@{
-			userID       = $User.SamAccountName
-			email        = $User.UserPrincipalName
-			model        = $_.model
-			deviceName   = $_.deviceName
-			serialNumber = $_.serialNumber
-		} | Select-Object -Property userID, email, model, deviceName, serialNumber | Export-Csv -Path "C:\Users\nefedod\OneDrive - MC CORMICK & COMPANY INC\2.csv" -NoTypeInformation -Delimiter ';' -Append -Force
+			Surname        = $UserID.Surname
+			Manufacturer   = $Device.Manufacturer
+			Model          = $Device.Model
+			"s/n"          = $Device.SerialNumber
+			hostname       = $Device.DeviceName
+			Build          = $Device.OSVersion
+			"Sync Time"    = $Device.LastSyncDateTime
+			"Free Storage" = [math]::Round($Device.FreeStorageSpaceInBytes/1GB,2)
+		} | Select-Object -Property * | Export-Csv -Path "D:\folder\2.csv" -Encoding UTF8 -NoTypeInformation -Delimiter ';' -Append -Force
+	}
+}
+
+# Export iPad properties
+# https://www.powershellgallery.com/packages/Microsoft.Graph.DeviceManagement
+# https://www.powershellgallery.com/packages/Microsoft.Graph
+$UserIDs = Get-ADUser -SearchBase "OU=RU,OU=Corp,DC=fr,DC=COMPANY,DC=lcl" -Filter * -Properties * | Where-Object {($_.employeeType -ne "SERVICE") -and $_.Enabled}
+foreach ($UserID in $UserIDs)
+{
+	$Devices = Get-MgDeviceManagementManagedDevice -All -Filter "OperatingSystem eq 'iOS' and UserPrincipalName eq '$($UserID.UserPrincipalName)'" -Property *
+	foreach ($Device in $Devices)
+	{
+		[PSCustomObject]@{
+			Surname        = $UserID.Surname
+			Model          = $Device.Model
+			"s/n"          = $Device.SerialNumber
+			Build          = [System.Version]$Device.OSVersion
+			"Sync Time"    = $Device.LastSyncDateTime
+		} | Select-Object -Property * | Export-Csv -Path "D:\folder\2.csv" -Encoding UTF8 -NoTypeInformation -Delimiter ';' -Append -Force
+	}
+}
+
+Get-MgDeviceManagementManagedDevice -Filter "OperatingSystem eq 'Windows' and UserPrincipalName eq 'userID@COMPANY.com'" -All -PageSize 100 -Property * -Top 100 | fl *
+Get-MgDeviceManagementManagedDevice -Filter "OperatingSystem eq 'iOS' and UserPrincipalName eq 'userID@COMPANY.com'" -All -PageSize 100 -Property * -Top 100 | fl *
+Get-MgDeviceManagementDetectedAppManagedDevice -Filter "deviceName eq 'PC_Name'" -Property * -all
+
+
+# Export installed apps from each laptop
+# https://www.powershellgallery.com/packages/Microsoft.Graph.DeviceManagement
+# https://www.powershellgallery.com/packages/Microsoft.Graph
+if (-not (Test-Path -Path D:\folder))
+{
+	New-Item -Path D:\folder -ItemType Directory -Force
+}
+Clear-Variable -Name allDetectedApps -ErrorAction Ignore
+$allDetectedApps = @()
+$UserIDs = Get-ADUser -SearchBase "OU=RU,OU=Corp,DC=fr,DC=COMPANY,DC=lcl" -Filter * -Properties * | Where-Object {($_.employeeType -ne "SERVICE") -and $_.Enabled}
+foreach ($UserID in $UserIDs)
+{
+	$Devices = Get-MgDeviceManagementManagedDevice -All -Filter "OperatingSystem eq 'Windows' and UserPrincipalName eq '$($UserID.UserPrincipalName)'" -Property *
+	foreach ($Device in $Devices)
+	{
+		Clear-Variable allDetectedApps -ErrorAction Ignore
+
+		$Uri = "https://graph.microsoft.com/beta/deviceManagement/managedDevices/$($Device.id)/detectedApps?$top=1000&$skip=0"
+		do
+		{
+			$response = Invoke-MgGraphRequest -Uri $Uri -Method GET
+			$allDetectedApps += $response.value
+			$uri = $response.'@odata.nextLink'
+		}
+		while ($uri)
+
+		$allDetectedApps | ForEach-Object {
+			[PSCustomObject]@{
+				Application = $_.displayName
+				Version     = $_.version
+			}
+		} | Select-Object -Property * | Export-Csv -Path "D:\folder\$($UserID.Surname).$($Device.deviceName).$($Device.serialNumber).csv" -Encoding UTF8 -NoTypeInformation -Delimiter ';' -Append -Force
+	}
+}
+
+# Export installed apps from each iPad
+# https://www.powershellgallery.com/packages/Microsoft.Graph.DeviceManagement
+# https://www.powershellgallery.com/packages/Microsoft.Graph
+if (-not (Test-Path -Path D:\folder))
+{
+	New-Item -Path D:\folder -ItemType Directory -Force
+}
+$UserIDs = Get-ADUser -SearchBase "OU=RU,OU=Corp,DC=fr,DC=COMPANY,DC=lcl" -Filter * -Properties * | Where-Object {($_.employeeType -ne "SERVICE") -and $_.Enabled}
+Clear-Variable -Name allDetectedApps -ErrorAction Ignore
+$allDetectedApps = @()
+foreach ($UserID in $UserIDs)
+{
+	$Devices = Get-MgDeviceManagementManagedDevice -All -Filter "Manufacturer eq 'Apple' and UserPrincipalName eq '$($UserID.UserPrincipalName)'" -Property *
+	foreach ($Device in $Devices)
+	{
+		Clear-Variable allDetectedApps -ErrorAction Ignore
+
+		$Uri = "https://graph.microsoft.com/beta/deviceManagement/managedDevices/$($Device.id)/detectedApps?$top=1000&$skip=0"
+		do
+		{
+			$response = Invoke-MgGraphRequest -Uri $Uri -Method GET
+			$allDetectedApps += $response.value
+			$uri = $response.'@odata.nextLink'
+		}
+		while ($uri)
+
+		$allDetectedApps | ForEach-Object {
+			[PSCustomObject]@{
+				Application = $_.displayName
+				Version     = $_.version
+			}
+		} | Select-Object -Property * | Export-Csv -Path "D:\folder\$($UserID.Surname).$($Device.deviceName).$($Device.SerialNumber).csv" -Encoding UTF8 -NoTypeInformation -Delimiter ';' -Append -Force
+	}
+}
+
+# Get installed apps list for user within Intune Device ID
+$allDetectedApps = @()
+$Uri = "https://graph.microsoft.com/beta/deviceManagement/managedDevices/374690b6-f43c-4c67-9068-50d94063d287/detectedApps?$top=1000&$skip=0"
+do
+{
+	$response = Invoke-MgGraphRequest -Uri $Uri -Method GET
+	$allDetectedApps += $response.value
+	$uri = $response.'@odata.nextLink'
+}
+while ($uri)
+
+$allDetectedApps | ForEach-Object {
+	[PSCustomObject]@{
+		Application = $_.displayName
+		Version     = $_.version
 	}
 }
