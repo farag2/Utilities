0e998108f1d75d42064c3c9d1ec99c8d21e15e59
Dmitry Nefedov <nefedovds@ya.ru>
2026-04-23 00:11:58 +0300

    Update nginx.sh

--- Linux/nginx.sh
+++ Linux/nginx.sh
@@ -39,51 +39,5 @@ TTL: 3600
 # Test configuration
 curl -I <domain>
 
-# Enable permanent redirection
-# sudo nano /etc/nginx/sites-available/<domain>
-
-```
-server {
-    listen 80;
-    server_name <subdomain.domain.com>;
-
-    # 301 Permanent Redirect
-    return 301 <new_link>;
-}
-```
-
-# sudo nano  /etc/nginx/sites-available/<domain>.conf
-
-```
-server {
-    listen 127.0.0.1:8443 ssl http2;
-    server_name <domain>;
-
-    ssl_certificate     /etc/letsencrypt/live/<domain>/fullchain.pem;
-    ssl_certificate_key /etc/letsencrypt/live/<domain>/privkey.pem;
-
-    ssl_protocols TLSv1.2 TLSv1.3;
-    ssl_ciphers HIGH:!aNULL:!MD5;
-
-    root /var/www/<domain>;
-    index index.html;
-
-    access_log /var/log/nginx/<domain>.access.log;
-    error_log  /var/log/nginx/<domain>.error.log;
-
-    location / {
-        try_files $uri $uri/ =404;
-    }
-}
-
-server {
-    listen 127.0.0.1:8080;
-    server_name <domain>;
-
-    # local HTTP → HTTPS redirect if needed for tools
-    return 301 https://<domain>$request_uri;
-}
-```
-
 # Enable nginix config
 sudo ln -s /etc/nginx/sites-available/<domain> /etc/nginx/sites-enabled/<domain>
