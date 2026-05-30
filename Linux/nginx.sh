f8654b56f01aeb711b166ec10991d03a096e4d83
Dmitry Nefedov <nefedovds@ya.ru>
2026-05-30 18:41:51 +0300

    Update nginx.sh

--- Linux/nginx.sh
+++ Linux/nginx.sh
@@ -41,3 +41,5 @@ curl -I <domain>
 
 # Enable nginix config
 sudo ln -s /etc/nginx/sites-available/<domain>.conf /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/<domain>.conf /etc/nginx/sites-enabled/<domain>.conf
+
