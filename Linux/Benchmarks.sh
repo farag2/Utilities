10f17b48945726e7bcd1a64aac0b7bf2f2f73cd9
Dmitry Nefedov <nefedovds@ya.ru>
2026-06-02 23:56:25 +0300

    Create Benchmarks.sh

---
+++ Linux/Benchmarks.sh
@@ -0,0 +1,30 @@
+# Get Ip information
+# https://github.com/vernette/ipregion
+bash <(wget -qO- https://ipregion.vrnt.xyz)
+
+# Censorship check
+# https://github.com/vernette/censorcheck
+bash <(wget -qO- https://github.com/vernette/censorcheck/raw/master/censorcheck.sh) --mode dpi
+bash <(wget -qO- https://github.com/vernette/censorcheck/raw/master/censorcheck.sh) --mode geoblock
+
+# Test connection to Russian iPerf servers
+# https://github.com/itdoginfo/russian-iperf3-servers
+bash <(wget -qO- https://github.com/itdoginfo/russian-iperf3-servers/raw/main/speedtest.sh)
+
+# Yet-Another-Bench-Script
+# https://github.com/masonr/yet-another-bench-script
+curl -sL yabs.sh | -s -- -4
+
+# Check Internet speed connection
+# https://bench.sh
+wget -qO- bench.sh | bash
+
+# IP quality
+# https://github.com/xykt/ScriptMenu
+bash <(curl -Ls https://Check.Place) -EI
+
+# CPU test
+sysbench cpu run --threads=1
+
+# Test server
+https://github.com/saveksme/multitest/
