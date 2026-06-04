020b7e417fc92702d38b49d048ff17267d1efa90
Dmitry Nefedov <nefedovds@ya.ru>
2026-06-05 01:41:18 +0300

    Update nginx.sh

--- Linux/nginx.sh
+++ Linux/nginx.sh
@@ -1,46 +1,26 @@
 # Install nginx
 sudo apt install nginx libnginx-mod-stream -y
-# check status
-sudo systemctl status nginx
-# start if not running
-sudo systemctl start nginx
-# start on boot
-sudo systemctl enable nginx
-# Install certbot and its dependencies
-sudo apt install certbot python3-certbot-nginx -y
 
 # Issue cert
+sudo apt install certbot python3-certbot-nginx -y
 sudo certbot certonly -d <domain>
 
 # Create a temp page
 sudo mkdir -p /var/www/<domain>
 echo "<domain>" | sudo tee /var/www/<domain>/index.html
 
-# Telemt service
-sudo systemctl start telemt
-sudo systemctl restart telemt
-sudo systemctl enable telemt
-sudo systemctl status telemt
-
-# Check telemt status
-sudo telemt /etc/telemt/telemt.toml
-
+# Check nginx status
 sudo nginx -t
 sudo systemctl restart nginx
 
-sudo journalctl -u telemt -f
-
-# In ISPmanager
-Name: subdomain_name
-Type: A
-Value: <VPS IP>
-TTL: 3600
-
-# Test configuration
-curl -I <domain>
+sudo systemctl start nginx
+sudo systemctl restart nginx
+sudo systemctl enable nginx
+sudo systemctl status nginx
 
 # check nginix enabled sites
 ls -l /etc/nginx/sites-enabled
+
 # Enable nginix config
 sudo ln -fs /etc/nginx/sites-available/<domain> /etc/nginx/sites-enabled/
 sudo ln -fs /etc/nginx/conf.d/<domain>
