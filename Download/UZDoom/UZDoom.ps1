428be15c32a7ff38eccba1e853f90a73f1270fd8
Dmitry Nefedov <nefedovds@ya.ru>
2026-08-16 17:41:41 +0300

    Update UZDoom.ps1

--- Download/UZDoom/UZDoom.ps1
+++ Download/UZDoom/UZDoom.ps1
@@ -1,3 +1,5 @@
+# uzdoom.exe -file doom2.wad brutalv22test3.pk3 EdayTest001.pk3 EdayMusic001.wad
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 if ($Host.Version.Major -eq 5)
